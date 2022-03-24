a3a6e688b9bb6af8b703d32c467dd8b7b745ba45
adrien.frossard <adrien.frossard@cgi.com>
2022-03-24 17:32:55 +0100

    fix():[FOR-297]block sharing to forms without questions' title

--- src/main/resources/public/ts/controllers/forms-list.ts
+++ src/main/resources/public/ts/controllers/forms-list.ts
@@ -218,32 +218,41 @@ export const formsListController = ng.controller('FormsListController', ['$scope
         }
     };
 
-    vm.shareForm = () : void => {
-        vm.forms.selected[0].generateShareRights();
-        template.open('lightbox', 'lightbox/form-sharing');
-        vm.display.lightbox.sharing = true;
-        window.setTimeout(async function () {
-            let contribs = document.querySelectorAll('[data-label="Contribuer"]');
-            let gestions = document.querySelectorAll('[data-label="Gérer"]');
-            for (let i = 1; i < contribs.length; i++) {
-                let contribValue = contribs[i].children[0].children[0] as HTMLInputElement;
-                let gestionValue = gestions[i].children[0].children[0] as HTMLInputElement;
-                contribValue.addEventListener('change', (e) => {
-                    let newValue = e.target as HTMLInputElement;
-                    if (!newValue.checked && gestionValue.checked) {
-                        gestionValue.checked = false;
-                    }
-                    $scope.safeApply();
-                });
-                gestionValue.addEventListener('change', (e) => {
-                    let newValue = e.target as HTMLInputElement;
-                    if (newValue.checked && !contribValue.checked) {
-                        contribValue.checked = true;
-                    }
-                    $scope.safeApply();
-                });
-            }
-        }, 500);
+    vm.shareForm = async() : Promise<void> => {
+        let questions =  Mix.castArrayAs(Question, await questionService.list(vm.forms.selected[0].id));
+        let wrongQuestions = questions.filter(question => !question.title);
+        if (wrongQuestions.length > 0){
+            notify.error(idiom.translate('formulaire.blockSharing'));
+
+        }
+        else{
+            vm.forms.selected[0].generateShareRights();
+            template.open('lightbox', 'lightbox/form-sharing');
+            vm.display.lightbox.sharing = true;
+            window.setTimeout(async function () {
+                let contribs = document.querySelectorAll('[data-label="Contribuer"]');
+                let gestions = document.querySelectorAll('[data-label="Gérer"]');
+                for (let i = 1; i < contribs.length; i++) {
+                    let contribValue = contribs[i].children[0].children[0] as HTMLInputElement;
+                    let gestionValue = gestions[i].children[0].children[0] as HTMLInputElement;
+                    contribValue.addEventListener('change', (e) => {
+                        let newValue = e.target as HTMLInputElement;
+                        if (!newValue.checked && gestionValue.checked) {
+                            gestionValue.checked = false;
+                        }
+                        $scope.safeApply();
+                    });
+                    gestionValue.addEventListener('change', (e) => {
+                        let newValue = e.target as HTMLInputElement;
+                        if (newValue.checked && !contribValue.checked) {
+                            contribValue.checked = true;
+                        }
+                        $scope.safeApply();
+                    });
+                }
+            }, 500);
+        }
+
     };
 
     vm.closeShareFormLightbox = () : void => {
