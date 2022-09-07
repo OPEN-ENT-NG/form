50d124751ba0e921b6edab6043cefb36b24860a2
Quentin Périé <quentin.perie@cgi.com>
2022-09-07 17:33:23 +0200

    feat(choices) : [FOR-262] add positioning for choices and matrix children

--- formulaire/src/main/resources/sql/016-matrix.sql
+++ formulaire/src/main/resources/sql/016-matrix.sql
@@ -1,5 +1,6 @@
 INSERT INTO formulaire.question_type(code, name) VALUES (10, 'MATRIX');
 
 ALTER TABLE formulaire.question
-    ADD COLUMN matrix_id    bigint,
+    ADD COLUMN matrix_id            bigint,
+    ADD COLUMN matrix_position      bigint,
     ADD CONSTRAINT fk_matrix_id FOREIGN KEY (matrix_id) REFERENCES formulaire.question (id) ON UPDATE NO ACTION ON DELETE CASCADE;
