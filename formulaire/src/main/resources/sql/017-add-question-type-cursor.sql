d9f9557d3ebdb9fe32e0fc10299cea25bdd2b7e9
CaroleHermouet <carole.hermouet@cgi.com>
2022-10-11 17:59:00 +0200

    feat(question-type-cursor): #FOR-456 WIP - Add new SQL table to manage specifics fields

--- formulaire/src/main/resources/sql/017-add-question-type-cursor.sql
+++ formulaire/src/main/resources/sql/017-add-question-type-cursor.sql
@@ -1,9 +1,13 @@
 INSERT INTO formulaire.question_type(code, name)
 VALUES (11, 'CURSOR');
 
-ALTER TABLE formulaire.question
-    ADD COLUMN cursor_min_val           bigint,
-    ADD COLUMN cursor_max_val           bigint,
-    ADD COLUMN cursor_step              bigint,
-    ADD COLUMN cursor_label_min_val     VARCHAR,
-    ADD COLUMN cursor_label_max_val     VARCHAR;
+CREATE TABLE formulaire.question_specific_field (
+       id                       bigserial PRIMARY KEY,
+       question_id              bigint NOT NULL,
+       cursor_min_val           bigint NOT NULL,
+       cursor_max_val           bigint NOT NULL,
+       cursor_step              bigint NOT NULL,
+       cursor_label_min_val     VARCHAR,
+       cursor_label_max_val     VARCHAR,
+       CONSTRAINT fk_question_id FOREIGN KEY (question_id) REFERENCES formulaire.question (id) ON UPDATE NO ACTION ON DELETE CASCADE
+);
