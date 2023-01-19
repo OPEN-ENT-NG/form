77d0e818ba04b25a69920ccbe93a95a39afffd36
CaroleHermouet <carole.hermouet@cgi.com>
2023-01-19 15:24:08 +0100

    feat(question-type-cursor): #FOR-525 init cursor response to minimum

--- formulaire/src/main/resources/public/ts/directives/question/respond-question-item.ts
+++ formulaire/src/main/resources/public/ts/directives/question/respond-question-item.ts
@@ -187,8 +187,8 @@ export const respondQuestionItem: Directive = ng.directive('respondQuestionItem'
                     }
                 }
 
-                if (vm.question.question_type === Types.CURSOR && typeof vm.responses.all[0].answer == "string") {
-                    vm.responses.all[0].answer = Number(vm.question.cursor_min_val);
+                if (vm.question.question_type === Types.CURSOR) {
+                    vm.responses.all[0].answer = vm.question.cursor_min_val;
                 }
 
                 $scope.$apply();
