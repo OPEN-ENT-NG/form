5d1781542d8911e97a99675d43fdf205370837ce
Quentin Périé <perieq@hotmail.fr>
2023-02-21 11:03:39 +0100

    feat(models): [#FOR-362] add back models (#201)

--- formulaire/src/main/java/fr/openent/formulaire/controllers/QuestionChoiceController.java
+++ formulaire/src/main/java/fr/openent/formulaire/controllers/QuestionChoiceController.java
@@ -78,7 +78,7 @@ public class QuestionChoiceController extends ControllerHelper {
             }
 
             // Check choice type validity
-            if (!choice.getString(TYPE).equals(ChoiceTypes.TXT.getName())) {
+            if (!choice.getString(TYPE).equals(ChoiceTypes.TXT.getValue())) {
                 String message = "[Formulaire@createQuestionChoice] Invalid choice type : " + choice.getString(TYPE);
                 log.error(message);
                 badRequest(request, message);
@@ -167,7 +167,7 @@ public class QuestionChoiceController extends ControllerHelper {
             }
 
             // Check choice type validity
-            if (!choice.getString(TYPE).equals(ChoiceTypes.TXT.getName())) {
+            if (!choice.getString(TYPE).equals(ChoiceTypes.TXT.getValue())) {
                 String message = "[Formulaire@updateQuestionChoice] Invalid choice type : " + choice.getString(TYPE);
                 log.error(message);
                 badRequest(request, message);
