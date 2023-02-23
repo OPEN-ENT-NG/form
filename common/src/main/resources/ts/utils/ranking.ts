c9abe01c7b3ea86a7db45f1fc9707dffbbe87be7
Carole Payen <carole.hermouet@cgi.com>
2023-02-23 11:09:07 +0100

    feat(question-type-ranking): #FOR-540 WIP

--- common/src/main/resources/ts/utils/ranking.ts
+++ common/src/main/resources/ts/utils/ranking.ts
@@ -2,25 +2,21 @@ import {Response, Responses} from "@common/models";
 
 export class RankingUtils {
     static onEndRankingDragAndDrop = (evt: any, responses: Responses): boolean => {
-        let oldIndex = evt.oldIndex;
-        let newIndex = evt.newIndex;
-        let indexes = RankingUtils.getStartEndIndexes(newIndex, oldIndex);
+        let oldIndex: number = evt.oldIndex;
+        let newIndex: number = evt.newIndex;
+        let indexes: any = RankingUtils.getStartEndIndexes(newIndex, oldIndex);
 
-        RankingUtils.updateChoicePositionsAfter(responses, true, indexes.goUp, indexes.startIndex, indexes.endIndex);
+        RankingUtils.updateChoicePositions(responses, indexes.goUp, indexes.startIndex, indexes.endIndex);
 
         responses.all.sort((a: Response, b: Response) => a.choice_position - b.choice_position);
         return false;
     }
 
-    static updateChoicePositionsAfter = (responses: Responses, isAdd: boolean, goUp: boolean, startIndex: number, endIndex?: number): void => {
-        endIndex = responses.all.length;
+    static updateChoicePositions = (responses: Responses, goUp: boolean, startIndex: number, endIndex?: number): void => {
+        endIndex = endIndex ? endIndex : responses.all.length;
         for (let i = startIndex; i < endIndex; i++) {
             let elt = responses.all[i];
-            if (goUp === null) {
-                isAdd ? elt.choice_position++ : elt.choice_position--;
-            } else {
-                goUp ? elt.choice_position++ : elt.choice_position--;
-            }
+            goUp ? elt.choice_position++ : elt.choice_position--;
         }
     };
 
