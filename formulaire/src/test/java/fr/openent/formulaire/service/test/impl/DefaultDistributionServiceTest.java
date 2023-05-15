fb5f5649be0406abb805afacaa15e3db9f3a7545
BOES <gregoryboes33@gmail.com>
2023-05-15 11:45:34 +0200

    feat(DistributionService) : #FOR-624 transform duplicateWithResponses methode's signature with future

--- formulaire/src/test/java/fr/openent/formulaire/service/test/impl/DefaultDistributionServiceTest.java
+++ formulaire/src/test/java/fr/openent/formulaire/service/test/impl/DefaultDistributionServiceTest.java
@@ -1,5 +1,6 @@
 package fr.openent.formulaire.service.test.impl;
 
+import fr.openent.form.core.constants.DistributionStatus;
 import fr.openent.formulaire.service.impl.DefaultDistributionService;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonArray;
@@ -8,13 +9,9 @@ import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 import org.entcore.common.sql.Sql;
-import org.entcore.common.user.UserInfos;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static fr.openent.form.core.constants.Constants.NB_NEW_LINES;
 import static fr.openent.form.core.constants.EbFields.FORMULAIRE_ADDRESS;
@@ -109,5 +106,124 @@ public class DefaultDistributionServiceTest {
             async.complete();
         });
         defaultDistributionService.listByFormAndStatusAndQuestion(FORM_ID, STATUS, QUESTION_ID,NB_LINES,null);
+    }
+
+    @Test
+    public void testDuplicateWithResponses_DistributionIdOk(TestContext ctx) {
+        Async async = ctx.async();
+        String expectedQuery =
+                "WITH newDistrib AS (" +
+                        "INSERT INTO " + DISTRIBUTION_TABLE + " (form_id, sender_id, sender_name, " +
+                        "responder_id, responder_name, status, date_sending, date_response, active, original_id) " +
+                        "SELECT form_id, sender_id, sender_name, responder_id, responder_name, ?, " +
+                        "date_sending, date_response, active, id FROM " + DISTRIBUTION_TABLE + " WHERE id = ? RETURNING *" +
+                        "), " +
+                        "newResponses AS (" +
+                        "INSERT INTO " + RESPONSE_TABLE + " (question_id, answer, responder_id, choice_id, distribution_id, original_id, choice_position, custom_answer) " +
+                        "SELECT question_id, answer, responder_id, choice_id, (SELECT id FROM newDistrib), id, choice_position, custom_answer " +
+                        "FROM " + RESPONSE_TABLE + " WHERE distribution_id = ? RETURNING *" +
+                        ")," +
+                        "newResponseFiles AS (" +
+                        "INSERT INTO " + RESPONSE_FILE_TABLE + " (id, response_id, filename, type) " +
+                        "SELECT id, (SELECT id FROM newResponses WHERE original_id = response_id), filename, type " +
+                        "FROM " + RESPONSE_FILE_TABLE + " WHERE response_id IN (SELECT original_id FROM newResponses)" +
+                        ")" +
+                        "SELECT * FROM newDistrib;";
+        JsonArray expectedParams = new JsonArray().add(DistributionStatus.ON_CHANGE).add(DISTRIBUTION_ID).add(DISTRIBUTION_ID);
+
+        vertx.eventBus().consumer(FORMULAIRE_ADDRESS, message -> {
+            JsonObject body = (JsonObject) message.body();
+            ctx.assertEquals(PREPARED, body.getString(ACTION));
+            ctx.assertEquals(expectedQuery, body.getString(STATEMENT));
+            ctx.assertEquals(expectedParams.toString(), body.getJsonArray(VALUES).toString());
+            async.complete();
+        });
+        defaultDistributionService.duplicateWithResponses(DISTRIBUTION_ID)
+                .onSuccess(result -> async.complete());
+
+        async.awaitSuccess(10000);
+    }
+
+    @Test
+    public void testDuplicateWithResponses_WrongDistributionId(TestContext ctx) {
+        Async async = ctx.async();
+        String expectedQuery =
+                "WITH newDistrib AS (" +
+                        "INSERT INTO " + DISTRIBUTION_TABLE + " (form_id, sender_id, sender_name, " +
+                        "responder_id, responder_name, status, date_sending, date_response, active, original_id) " +
+                        "SELECT form_id, sender_id, sender_name, responder_id, responder_name, ?, " +
+                        "date_sending, date_response, active, id FROM " + DISTRIBUTION_TABLE + " WHERE id = ? RETURNING *" +
+                        "), " +
+                        "newResponses AS (" +
+                        "INSERT INTO " + RESPONSE_TABLE + " (question_id, answer, responder_id, choice_id, distribution_id, original_id, choice_position, custom_answer) " +
+                        "SELECT question_id, answer, responder_id, choice_id, (SELECT id FROM newDistrib), id, choice_position, custom_answer " +
+                        "FROM " + RESPONSE_TABLE + " WHERE distribution_id = ? RETURNING *" +
+                        ")," +
+                        "newResponseFiles AS (" +
+                        "INSERT INTO " + RESPONSE_FILE_TABLE + " (id, response_id, filename, type) " +
+                        "SELECT id, (SELECT id FROM newResponses WHERE original_id = response_id), filename, type " +
+                        "FROM " + RESPONSE_FILE_TABLE + " WHERE response_id IN (SELECT original_id FROM newResponses)" +
+                        ")" +
+                        "SELECT * FROM newDistrib;";
+
+        String BadId = "BadId";
+        JsonArray expectedParams = new JsonArray().add(DistributionStatus.ON_CHANGE).add(BadId).add(BadId);
+
+        vertx.eventBus().consumer(FORMULAIRE_ADDRESS, message -> {
+            JsonObject body = (JsonObject) message.body();
+            ctx.assertEquals(PREPARED, body.getString(ACTION));
+            ctx.assertEquals(expectedQuery, body.getString(STATEMENT));
+            ctx.assertEquals(expectedParams.toString(), body.getJsonArray(VALUES).toString());
+            async.complete();
+        });
+        defaultDistributionService.duplicateWithResponses(BadId)
+                .onSuccess(result -> {
+                    ctx.assertNull(result);
+                    async.complete();
+                });
+
+        async.awaitSuccess(10000);
+    }
+
+
+    @Test
+    public void testDuplicateWithResponses_DistributionIdNull(TestContext ctx) {
+        Async async = ctx.async();
+        String expectedQuery =
+                "WITH newDistrib AS (" +
+                        "INSERT INTO " + DISTRIBUTION_TABLE + " (form_id, sender_id, sender_name, " +
+                        "responder_id, responder_name, status, date_sending, date_response, active, original_id) " +
+                        "SELECT form_id, sender_id, sender_name, responder_id, responder_name, ?, " +
+                        "date_sending, date_response, active, id FROM " + DISTRIBUTION_TABLE + " WHERE id = ? RETURNING *" +
+                        "), " +
+                        "newResponses AS (" +
+                        "INSERT INTO " + RESPONSE_TABLE + " (question_id, answer, responder_id, choice_id, distribution_id, original_id, choice_position, custom_answer) " +
+                        "SELECT question_id, answer, responder_id, choice_id, (SELECT id FROM newDistrib), id, choice_position, custom_answer " +
+                        "FROM " + RESPONSE_TABLE + " WHERE distribution_id = ? RETURNING *" +
+                        ")," +
+                        "newResponseFiles AS (" +
+                        "INSERT INTO " + RESPONSE_FILE_TABLE + " (id, response_id, filename, type) " +
+                        "SELECT id, (SELECT id FROM newResponses WHERE original_id = response_id), filename, type " +
+                        "FROM " + RESPONSE_FILE_TABLE + " WHERE response_id IN (SELECT original_id FROM newResponses)" +
+                        ")" +
+                        "SELECT * FROM newDistrib;";
+
+        String BadId = "BadId";
+        JsonArray expectedParams = new JsonArray().add(DistributionStatus.ON_CHANGE).addNull().addNull();
+
+        vertx.eventBus().consumer(FORMULAIRE_ADDRESS, message -> {
+            JsonObject body = (JsonObject) message.body();
+            ctx.assertEquals(PREPARED, body.getString(ACTION));
+            ctx.assertEquals(expectedQuery, body.getString(STATEMENT));
+            ctx.assertEquals(expectedParams.toString(), body.getJsonArray(VALUES).toString());
+            async.complete();
+        });
+        defaultDistributionService.duplicateWithResponses(null)
+                .onSuccess(result -> {
+                    ctx.assertNull(result);
+                    async.complete();
+                });
+
+        async.awaitSuccess(10000);
     }
 }
