fa140dee2b517b62e7272b0f36665bd254857e1a
Carole Payen <carole.hermouet@cgi.com>
2023-06-23 15:38:14 +0200

    feat(img-singleanswer): #FOR-670 manage mandatory label

--- formulaire/src/main/resources/public/ts/controllers/form-editor.ts
+++ formulaire/src/main/resources/public/ts/controllers/form-editor.ts
@@ -170,9 +170,11 @@ export const formEditorController = ng.controller('FormEditorController', ['$sco
                 }
             }
 
-            // Check choice.image and choice.value for questions of type Types.MULTIPLEANSWER & types.SINGLEANSWER
-            let questionTypeMultipleanswers: Question[] = vm.formElements.getAllQuestions().filter((q: Question) => q.question_type == Types.MULTIPLEANSWER);
-            const foundChoice = (<any>questionTypeMultipleanswers).flatMap((question: Question) => question.choices.all)
+            // Check choice.image and choice.value for questions of type MULTIPLEANSWER & SINGLEANSWER
+            let multipleanswerOrSingleanswer: Question[] =
+                vm.formElements.getAllQuestions().filter((q: Question) => (q.question_type == Types.MULTIPLEANSWER)
+                    || (q.question_type == Types.SINGLEANSWERRADIO));
+            const foundChoice = (<any>multipleanswerOrSingleanswer).flatMap((question: Question) => question.choices.all)
                 .find((choice: QuestionChoice) => (choice.image && !choice.value));
 
             if (foundChoice) {
