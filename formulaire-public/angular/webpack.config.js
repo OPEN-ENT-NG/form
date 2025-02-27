3d1b2e654a418cada9d3262b8559a3e22a11dd7c
Quentin Périé <perieq@hotmail.fr>
2025-02-27 18:11:11 +0100

    chore(): [#FOR-868] change architecture to prepare React migration (#427)
    * chore(): [#FOR-868] change architecture to prepare React migration
    
    * chore(pipeline): [#FOR-868] fix git workflow

---
+++ formulaire-public/angular/webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path');
+
+module.exports = {
+    entry: {
+        application: './src/ts/app.ts',
+        behaviours: './src/ts/behaviours.ts'
+    },
+    output: {
+        filename: '[name].js',
+        path: __dirname + 'dest'
+    },
+    externals: {
+        "entcore/entcore": "entcore",
+        "entcore": "entcore",
+        "moment": "entcore",
+        "underscore": "entcore",
+        "jquery": "entcore",
+        "angular": "angular"
+    },
+    resolve: {
+        modulesDirectories: ['node_modules'],
+        root: path.resolve(__dirname),
+        extensions: ['', '.ts', '.js'],
+        alias: {
+            "@common": path.resolve(__dirname, "common/src/main/resources/ts")
+        }
+    },
+    devtool: "source-map",
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader'
+            }
+        ]
+    }
+};
