c2a94c9c752a79c4cf1cb9af7dc8afb794440201
Quentin Périé <perieq@hotmail.fr>
2025-03-12 11:05:13 +0100

    chore(): fix start module

--- formulaire/frontend/src/core/constants.ts
+++ formulaire/frontend/src/core/constants.ts
@@ -1,3 +1,11 @@
+import { Theme, ToastPosition } from "react-toastify";
+
 export const FORMULAIRE = "formulaire";
 
 export const DEFAULT_THEME = "default";
+
+export const TOAST_CONFIG = {
+  position: "top-right" as ToastPosition,
+  autoClose: 5000,
+  theme: "light" as Theme,
+};
