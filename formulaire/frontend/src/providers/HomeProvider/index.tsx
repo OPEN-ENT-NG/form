b289048f9e83b0feef348989ece47a3c864090b8
Alex Delaveau <57497688+Alex-Delaveau@users.noreply.github.com>
2025-03-14 09:22:10 +0100

    feat(modal): #FOR-886 add create/rename folder modal in homepage (#430)

---
+++ formulaire/frontend/src/providers/HomeProvider/index.tsx
@@ -0,0 +1,49 @@
+import { FC, createContext, useContext, useMemo, useState } from "react";
+import {
+  DisplayModalsState,
+  HomeProviderContextType,
+  HomeProviderProps,
+} from "./types";
+import { initialDisplayModalsState } from "./utils";
+import { MODAL_TYPE } from "~/core/enums";
+import { Folder } from "~/core/models/folders/types";
+
+const HomeProviderContext = createContext<HomeProviderContextType | null>(null);
+
+export const useHomeProvider = () => {
+  const context = useContext(HomeProviderContext);
+  if (!context) {
+    throw new Error("useHomeProvider must be used within a HomeProvider");
+  }
+  return context;
+};
+
+export const HomeProvider: FC<HomeProviderProps> = ({ children }) => {
+  const [currentFolder, setCurrentFolder] = useState<Folder | null>(null);
+  const [displayModals, setDisplayModals] = useState<DisplayModalsState>(
+    initialDisplayModalsState,
+  );
+
+  const handleDisplayModal = (modalType: MODAL_TYPE) =>
+    setDisplayModals((prevState) => ({
+      ...prevState,
+      [modalType]: !prevState[modalType],
+    }));
+
+  const value = useMemo<HomeProviderContextType>(
+    () => ({
+      displayModals,
+      setDisplayModals,
+      handleDisplayModal,
+      currentFolder,
+      setCurrentFolder,
+    }),
+    [displayModals, currentFolder],
+  );
+
+  return (
+    <HomeProviderContext.Provider value={value}>
+      {children}
+    </HomeProviderContext.Provider>
+  );
+};
