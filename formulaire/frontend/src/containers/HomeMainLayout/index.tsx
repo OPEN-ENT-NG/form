2879624b993181fd50216ddec77802748fad7607
Thomas Sevagen <thomas.sevagen@cgi.com>
2025-03-17 19:28:05 +0100

    fix: update Box import source in HomeMainLayout

--- formulaire/frontend/src/containers/HomeMainLayout/index.tsx
+++ formulaire/frontend/src/containers/HomeMainLayout/index.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { Box } from "@mui/material";
+import { Box } from "@cgi-learning-hub/ui";
 import { useHomeProvider } from "~/providers/HomeProvider";
 import {
   sidebarStyle,
