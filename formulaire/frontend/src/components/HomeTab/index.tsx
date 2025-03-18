3d0745b2bdb61c233326474502d7324bfba920a3
Thomas Sevagen <thomas.sevagen@cgi.com>
2025-03-18 10:26:30 +0100

    feat(sidebar) : mainlayout + sidebarlayout + treeview-impl + tab (#434)
    * feat: implement HomeTabs component and update folder modal functionality
    
    * feat(sidebar) : mainlayout+sidebarlayout+treeview-impl+tab
    
    * fix: update Box import source in HomeMainLayout
    
    * fix(form): fix review

---
+++ formulaire/frontend/src/components/HomeTab/index.tsx
@@ -0,0 +1,23 @@
+import { FC } from "react";
+import { Tab, Tabs } from "@cgi-learning-hub/ui";
+import { HomeTabsProps } from "./types";
+import { HomeTabState } from "~/providers/HomeProvider/enums";
+import { useTranslation } from "react-i18next";
+import { FORMULAIRE } from "~/core/constants";
+
+export const HomeTabs: FC<HomeTabsProps> = ({ value, setValue }) => {
+  const { t } = useTranslation(FORMULAIRE);
+  const handleChange = (_: React.SyntheticEvent, newValue: HomeTabState) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Tabs value={value} onChange={handleChange}>
+      <Tab value={HomeTabState.FORMS} label={t("formulaire.tab.list")} />
+      <Tab
+        value={HomeTabState.RESPONSES}
+        label={t("formulaire.tab.responses")}
+      />
+    </Tabs>
+  );
+};
---
+++ formulaire/frontend/src/containers/HomeSidebar/utils.ts
@@ -0,0 +1,52 @@
+import { Folder } from "~/core/models/folders/types";
+import { CustomTreeViewItem, ICON_TYPE } from "@cgi-learning-hub/ui";
+
+const SYSTEM_FOLDERS = {
+  MY_FORMS: 1,
+  SHARED: 2,
+  TRASH: 3,
+};
+
+export const buildFolderTree = (folders: Folder[]): CustomTreeViewItem[] => {
+  const rootFolders = folders.filter(
+    (folder) =>
+      folder.id === SYSTEM_FOLDERS.MY_FORMS ||
+      folder.id === SYSTEM_FOLDERS.SHARED ||
+      folder.id === SYSTEM_FOLDERS.TRASH,
+  );
+
+  const buildNestedFolders = (parentId: number): CustomTreeViewItem[] => {
+    return folders
+      .filter(
+        (folder) => folder.parent_id === parentId && folder.id !== parentId,
+      )
+      .map((folder) => {
+        const childFolders = buildNestedFolders(folder.id);
+
+        return {
+          internalId: folder.id.toString(),
+          label: folder.name,
+          iconType: ICON_TYPE.FOLDER,
+          ...(childFolders.length > 0 ? { children: childFolders } : {}),
+        };
+      });
+  };
+
+  return rootFolders.map((rootFolder) => {
+    const childFolders = buildNestedFolders(rootFolder.id);
+
+    const iconType =
+      rootFolder.id === SYSTEM_FOLDERS.SHARED
+        ? ICON_TYPE.SHARE
+        : rootFolder.id === SYSTEM_FOLDERS.TRASH
+        ? ICON_TYPE.TRASH
+        : ICON_TYPE.FOLDER;
+
+    return {
+      internalId: rootFolder.id.toString(),
+      label: rootFolder.name,
+      iconType,
+      children: childFolders.length > 0 ? childFolders : [],
+    };
+  });
+};
