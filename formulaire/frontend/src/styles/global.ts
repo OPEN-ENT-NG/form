9702a0480e5407ab6e1afc71da2c3cdd8fd59dd7
Thomas Sevagen <thomas.sevagen@cgi.com>
2025-03-18 10:26:30 +0100

    feat(sidebar) : mainlayout + sidebarlayout + treeview-impl + tab (#434)
    * feat: implement HomeTabs component and update folder modal functionality
    
    * feat(sidebar) : mainlayout+sidebarlayout+treeview-impl+tab
    
    * fix: update Box import source in HomeMainLayout
    
    * fix(form): fix review

--- formulaire/frontend/src/styles/global.ts
+++ formulaire/frontend/src/styles/global.ts
@@ -1,5 +1,7 @@
 export const globalOverrideStyles = {
   "main.container-fluid": {
-    backgroundColor: "transparent ! important",
+    padding: "0 ! important",
+    minHeight: "calc(100vh - 71px) ! important",
+    height: "calc(100vh - 71px) ! important",
   },
 };
