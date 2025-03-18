d42e30fd584e6d2cb9daeba4d73834150029a0e1
Thomas Sevagen <thomas.sevagen@cgi.com>
2025-03-18 12:51:25 +0100

    feat(modal): implement modal provider and update modal handling in header and sidebar

--- formulaire/frontend/src/providers/HomeProvider/types.ts
+++ formulaire/frontend/src/providers/HomeProvider/types.ts
@@ -1,5 +1,4 @@
 import { ReactNode } from "react";
-import { ModalType } from "src/core/enums";
 import { Folder } from "~/core/models/folders/types";
 import { HomeTabState } from "./enums";
 
@@ -7,15 +6,7 @@ export interface HomeProviderProps {
   children: ReactNode;
 }
 
-export interface DisplayModalsState {
-  [ModalType.FOLDER_CREATE]: boolean;
-  [ModalType.FOLDER_RENAME]: boolean;
-}
-
 export type HomeProviderContextType = {
-  displayModals: DisplayModalsState;
-  setDisplayModals: (value: DisplayModalsState) => void;
-  handleDisplayModal: (modalType: ModalType) => void;
   currentFolder: Folder | null;
   setCurrentFolder: (value: Folder | null) => void;
   tab: HomeTabState;
