c8e3a60f5608e55568546ebc9ae78f33db8d5462
Alex Delaveau <57497688+Alex-Delaveau@users.noreply.github.com>
2025-03-20 11:25:55 +0100

    feat(modal): #FOR-886 add delete folders/forms modal in homepage (#433)

---
+++ formulaire/frontend/src/containers/DeleteModal/utils.ts
@@ -0,0 +1,33 @@
+import { Folder } from "~/core/models/folders/types";
+import { Form } from "~/core/models/forms/types";
+import { DeleteModalVariant } from "./enum";
+import { deleteModalContent } from "./types";
+
+export const getCorrectValue = (
+  forms: Form[],
+  folders: Folder[],
+): DeleteModalVariant => {
+  if (forms.length === 1 && !folders.length) {
+    return DeleteModalVariant.FORM_UNIQUE;
+  }
+  if (forms.length && !folders.length) {
+    return DeleteModalVariant.FORM_MULTIPLE;
+  }
+  if (!forms.length && folders.length === 1) {
+    return DeleteModalVariant.FOLDER_UNIQUE;
+  }
+  if (!forms.length && folders.length) {
+    return DeleteModalVariant.FOLDER_MULTIPLE;
+  }
+  return DeleteModalVariant.FORM_FOLDER;
+};
+
+export const getTitle = (forms: Form[], folders: Folder[]) => {
+  const enumValue = getCorrectValue(forms, folders);
+  return deleteModalContent[enumValue].titleKey;
+};
+
+export const getText = (forms: Form[], folders: Folder[]) => {
+  const enumValue = getCorrectValue(forms, folders);
+  return deleteModalContent[enumValue].textKey;
+};
