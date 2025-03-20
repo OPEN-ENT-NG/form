3f4d1b610bbece00042110e0e43524b2f8aef1c2
Thomas Sevagen <thomas.sevagen@cgi.com>
2025-03-20 14:13:00 +0100

    feat(cpt): FOR-875 add create/update form modal (#436)
    * feat(FormPropModal): add modal component with create/update functionality and localization
    
    * feat(FormPropModal): enhance modal with image upload, date selection, and improved localization
    
    * wip
    
    * feat(FormPropModal): refactor date handling and improve checkbox state management
    
    * feat(Form): refactor form model and API integration; update folder imports and improve state management
    
    * feat(RgpdInfoBox, FormPropModal): fix review

---
+++ formulaire/frontend/src/core/models/form/types.ts
@@ -0,0 +1,66 @@
+export interface Form {
+  id: number;
+  title: string;
+  description: string;
+  picture: string;
+  owner_id: string;
+  owner_name: string;
+  date_creation: Date;
+  date_modification: Date;
+  date_opening: Date;
+  date_ending: Date;
+  sent: boolean;
+  collab: boolean;
+  reminded: boolean;
+  archived: boolean;
+  multiple: boolean;
+  anonymous: boolean;
+  is_public: boolean;
+  public_key: string;
+  response_notified: boolean;
+  editable: boolean;
+  displayed: boolean;
+  rgpd: boolean;
+  rgpd_goal: string;
+  rgpd_lifetime: number;
+  folder_id: number;
+  nb_elements: number;
+  nb_responses: number;
+  infoImg: InfoImg;
+}
+
+export interface InfoImg {
+  name: string;
+  type: string;
+  compatible: boolean;
+}
+
+export interface FormPayload {
+  anonymous: boolean;
+  archived: boolean;
+  collab: boolean;
+  date_creation: null;
+  date_ending: string | null;
+  date_modification: null;
+  date_opening: string;
+  description: string | null;
+  displayed: boolean;
+  editable: boolean;
+  folder_id: number;
+  id: number | null;
+  is_public: boolean;
+  multiple: boolean;
+  nb_responses: number;
+  owner_id: number | null;
+  owner_name: string | null;
+  picture: string | null;
+  public_key: string | null;
+  reminded: boolean;
+  response_notified: boolean;
+  rgpd: boolean;
+  rgpd_goal: string | null;
+  rgpd_lifetime: number;
+  selected: unknown | null;
+  sent: boolean;
+  title: string;
+}
