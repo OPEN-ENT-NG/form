68c41fc6e181aa217335e8f10cf0acf960c6fd4c
Alex Delaveau <57497688+Alex-Delaveau@users.noreply.github.com>
2025-03-20 11:37:59 +0100

    feat(folder): #FOR-887 #FOR-876 add home main layout and folder (#435)

--- formulaire/frontend/src/components/Breadcrumbs/index.tsx
+++ formulaire/frontend/src/components/Breadcrumbs/index.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 import { FormBreadcrumbsProps } from "./types";
 import { Breadcrumbs } from "@mui/material";
-import { Link, Typography } from "@cgi-learning-hub/ui";
+import { Box, Link, Typography } from "@cgi-learning-hub/ui";
 import { FormsIcon } from "../SVG/FormsIcon";
 import {
   FORM_COLOR,
@@ -21,13 +21,15 @@ export const FormBreadcrumbs: FC<FormBreadcrumbsProps> = ({
       separator={displaySeparator && (separator ?? null)}
       maxItems={3}
     >
-      <Link
-        underline="hover"
-        color={isHeader ? FORM_COLOR : GREY_DARKER_COLOR}
-        href="/"
-      >
-        <FormsIcon height={isHeader ? "5rem" : "2.3rem"} />
-      </Link>
+      {isHeader ? (
+        <Link underline="hover" color={FORM_COLOR} href="/">
+          <FormsIcon height="5rem" />
+        </Link>
+      ) : (
+        <Box color={GREY_DARKER_COLOR}>
+          <FormsIcon height="2.3rem" />
+        </Box>
+      )}
       {stringItems.map((stringItem) => (
         <Typography
           key={stringItem}
--- formulaire/frontend/src/containers/HomeMainLayout/index.tsx
+++ formulaire/frontend/src/containers/HomeMainLayout/index.tsx
@@ -1,5 +1,5 @@
-import { FC } from "react";
-import { Box } from "@cgi-learning-hub/ui";
+import { FC, useCallback } from "react";
+import { Box, FolderCard, SearchInput } from "@cgi-learning-hub/ui";
 import { useHome } from "~/providers/HomeProvider";
 import {
   sidebarStyle,
@@ -7,13 +7,49 @@ import {
   mainContentStyle,
   mainContentInnerStyle,
   HomeMainLayoutWrapper,
+  searchStyle,
+  resourceContainerStyle,
+  searchBarStyle,
 } from "./styles";
 import { HomeTabState } from "~/providers/HomeProvider/enums";
 import { HomeSidebar } from "../HomeSidebar";
 import { HomeMainLayoutProps } from "./types";
+import { Button, Switch } from "@mui/material";
+import { FormBreadcrumbs } from "~/components/Breadcrumbs";
+import { spaceBetweenBoxStyle } from "~/styles/boxStyles";
+
+import Grid from "@mui/material/Grid2";
+import { Folder } from "~/core/models/folders/types";
 
 export const HomeMainLayout: FC<HomeMainLayoutProps> = ({ headerHeight }) => {
-  const { tab } = useHome();
+  const {
+    tab,
+    selectedFolders,
+    setSelectedFolders,
+    folders,
+    currentFolder,
+    setCurrentFolder,
+  } = useHome();
+
+  const handleFolderSelect = useCallback(
+    (folder: Folder) => {
+      if (selectedFolders.some((f) => f.id === folder.id)) {
+        return setSelectedFolders(
+          selectedFolders.filter((f) => f.id !== folder.id),
+        );
+      }
+      return setSelectedFolders([...selectedFolders, folder]);
+    },
+    [selectedFolders, setSelectedFolders],
+  );
+
+  const filteredFolders = folders.filter(
+    (folder) => folder.parent_id === (currentFolder ? currentFolder.id : null),
+  );
+  const hasFilteredFolders = !!filteredFolders.length;
+  const breadcrumbsText = currentFolder?.name ? [currentFolder.name] : [];
+  const isSelectedFolder = (folder: Folder) =>
+    selectedFolders.some((f) => f.id === folder.id);
 
   return (
     <HomeMainLayoutWrapper headerHeight={headerHeight}>
@@ -25,7 +61,38 @@ export const HomeMainLayout: FC<HomeMainLayoutProps> = ({ headerHeight }) => {
         </Box>
       )}
       <Box sx={mainContentStyle}>
-        <Box sx={mainContentInnerStyle}>TODO</Box>
+        <Box sx={mainContentInnerStyle}>
+          <Box sx={searchStyle}>
+            <SearchInput placeholder="Search" sx={searchBarStyle} />
+            <Button variant="outlined">{"Organiser"}</Button>
+          </Box>
+          <Box sx={spaceBetweenBoxStyle}>
+            <FormBreadcrumbs stringItems={breadcrumbsText} />
+            <Switch />
+          </Box>
+
+          <Box sx={resourceContainerStyle}>
+            {hasFilteredFolders && (
+              <Grid container spacing={3}>
+                {filteredFolders.map((folder) => (
+                  <Grid size={4} key={folder.id}>
+                    <FolderCard
+                      title={folder.name}
+                      width="100%"
+                      onSelect={() => handleFolderSelect(folder)}
+                      onClick={() => setCurrentFolder(folder)}
+                      isSelected={isSelectedFolder(folder)}
+                      iconSize="3.2rem"
+                    />
+                  </Grid>
+                ))}
+              </Grid>
+            )}
+            <Grid container spacing={3}>
+              {"// Add the forms here"}
+            </Grid>
+          </Box>
+        </Box>
       </Box>
     </HomeMainLayoutWrapper>
   );
