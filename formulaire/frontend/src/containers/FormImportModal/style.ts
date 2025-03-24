777bfe1dafa9eb11514af31bc2ccdfda2e879ab2
Quentin Périé <perieq@hotmail.fr>
2025-03-24 11:55:17 +0100

    feat(import): [#FOR-946] add import modal (#440)
    * feat(import): [#FOR-946] add import modal WIP
    
    * rebase
    
    * rebase
    
    * feat(): [#FOR-946] update dropzone style
    
    * lint
    
    * feat(import): [#FOR-946] improve code

---
+++ formulaire/frontend/src/containers/FormImportModal/style.ts
@@ -0,0 +1,19 @@
+export const formImportModalStyle = {
+  width: "76rem",
+};
+
+export const formImportModalContentStyle = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  gap: "2rem",
+};
+
+export const formImportModalDropZoneStyle = {
+  textAlign: "center",
+  width: "30rem",
+};
+
+export const dropZoneSlotProps = {
+  root: { sx: { textAlign: "center" } },
+};
