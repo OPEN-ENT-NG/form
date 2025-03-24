a9098f22707bc2c5d454751cd31419fb5efe3dcb
Thomas Sevagen <thomas.sevagen@cgi.com>
2025-03-24 11:59:17 +0100

    feat(mediaLibrary): add medialibrary for imagePicker (#439)
    * wip
    
    * feat(date): add full date with time format and custom hook for formatting
    
    * feat(image-picker): enhance media library functionality and improve checkbox interactions
    
    * feat(mediaLibrary): add medialibrary for imagePicker
    
    * refactor(image-picker): remove commented code in ImagePickerMediaLibrary component

--- formulaire/frontend/src/hook/useFormatDateWithTime.ts
+++ formulaire/frontend/src/hook/useFormatDateWithTime.ts
@@ -1,16 +1,16 @@
 import { useTranslation } from "react-i18next";
-import { FORMULAIRE } from "~/core/constants";
+import { FORMULAIRE, FULL_DATE_WITH_TIME_FORMAT } from "~/core/constants";
+import dayjs from "dayjs";
+import "dayjs/locale/fr";
+import "dayjs/locale/en";
+
 export const useFormatDateWithTime = () => {
   const { t } = useTranslation(FORMULAIRE);
 
   return (date: string | Date | undefined): string => {
     if (!date) return "";
-    return new Intl.DateTimeFormat(t("formulaire.date.format.locale"), {
-      day: "2-digit",
-      month: "long",
-      year: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
-    }).format(new Date(date));
+    const locale = t("formulaire.date.format.locale");
+    dayjs.locale(locale);
+    return dayjs(date).format(FULL_DATE_WITH_TIME_FORMAT);
   };
 };
