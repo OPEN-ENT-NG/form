9e47d6d979b9c5562222eb004e23c829fcc9fb96
Thomas Sevagen <thomas.sevagen@cgi.com>
2025-03-25 16:47:57 +0100

    fix(form): update rename payload key for back

--- formulaire/frontend/src/core/models/form/types.ts
+++ formulaire/frontend/src/core/models/form/types.ts
@@ -63,7 +63,7 @@ export interface FormPayload {
   selected: unknown | null;
   sent: boolean;
   title: string;
-  isProgressBarDisplayed: boolean;
+  is_progress_bar_displayed: boolean;
 }
 
 export interface DuplicateFormPayload {
