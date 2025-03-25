b40e207d6454f1c1f556102752648a1adb3b96e1
Quentin Périé <perieq@hotmail.fr>
2025-03-25 09:46:16 +0100

    feat(): [#FOR-699, #FOR-596] add description and isProgressBArDisplayed to form props (#442)

--- common/src/main/java/fr/openent/form/core/constants/Fields.java
+++ common/src/main/java/fr/openent/form/core/constants/Fields.java
@@ -43,6 +43,7 @@ public class Fields {
     public static final String ID = "id";
     public static final String IMAGE = "image";
     public static final String IS_CUSTOM = "is_custom";
+    public static final String IS_PROGRESS_BAR_DISPLAYED = "is_progress_bar_displayed";
     public static final String IS_PUBLIC = "is_public";
     public static final String IS_NEXT_FORM_ELEMENT_DEFAULT = "is_next_form_element_default";
     public static final String MAIL = "mail";
