686d7e8446a18bbc226fb23c94a76424bca5f622
Thomas Sevagen <thomas.sevagen@cgi.com>
2025-03-26 16:40:45 +0100

    core(conf): add host & theme conf to front (#446)
    * feat(config): add HOST and THEME_PLATFORM constants and update related configurations
    
    * fix(html): move data attributes to root div for improved structure

--- formulaire/backend/src/main/java/fr/openent/formulaire/controllers/FormulaireController.java
+++ formulaire/backend/src/main/java/fr/openent/formulaire/controllers/FormulaireController.java
@@ -1,7 +1,21 @@
 package fr.openent.formulaire.controllers;
 
-import fr.wseduc.rs.Get;
+import java.util.Map;
+
+import org.entcore.common.controller.ControllerHelper;
+import org.entcore.common.events.EventStore;
+import org.entcore.common.http.filter.ResourceFilter;
+import org.entcore.common.http.filter.SuperAdminFilter;
+import org.vertx.java.core.http.RouteMatcher;
+
+import static fr.openent.form.core.constants.ConfigFields.AUTH;
+import static fr.openent.form.core.constants.ConfigFields.HOST;
+import static fr.openent.form.core.constants.ConfigFields.NODE_PDF_GENERATOR;
+import static fr.openent.form.core.constants.ConfigFields.THEME_PLATFORM;
+import static fr.openent.form.core.constants.ConsoleRights.ACCESS_RIGHT;
+import static fr.openent.form.core.enums.Events.ACCESS;
 import fr.wseduc.rs.ApiDoc;
+import fr.wseduc.rs.Get;
 import fr.wseduc.security.ActionType;
 import fr.wseduc.security.SecuredAction;
 import io.vertx.core.Vertx;
@@ -9,18 +23,6 @@ import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
-import org.entcore.common.controller.ControllerHelper;
-import org.entcore.common.events.EventStore;
-import org.entcore.common.http.filter.ResourceFilter;
-import org.entcore.common.http.filter.SuperAdminFilter;
-import org.vertx.java.core.http.RouteMatcher;
-
-import java.util.Map;
-
-import static fr.openent.form.core.enums.Events.ACCESS;
-import static fr.openent.form.core.constants.ConfigFields.AUTH;
-import static fr.openent.form.core.constants.ConfigFields.NODE_PDF_GENERATOR;
-import static fr.openent.form.core.constants.ConsoleRights.ACCESS_RIGHT;
 
 public class FormulaireController extends ControllerHelper {
     private static final Logger log = LoggerFactory.getLogger(FormulaireController.class);
@@ -32,7 +34,8 @@ public class FormulaireController extends ControllerHelper {
     }
 
     @Override
-    public void init(Vertx vertx, JsonObject config, RouteMatcher rm, Map<String, fr.wseduc.webutils.security.SecuredAction> securedActions) {
+    public void init(Vertx vertx, JsonObject config, RouteMatcher rm,
+            Map<String, fr.wseduc.webutils.security.SecuredAction> securedActions) {
         super.init(vertx, config, rm, securedActions);
     }
 
@@ -41,10 +44,14 @@ public class FormulaireController extends ControllerHelper {
     @SecuredAction(ACCESS_RIGHT)
     public void render(HttpServerRequest request) {
         final String view = request.params().get("view");
+        JsonObject safeConfig = config.copy();
+        JsonObject params = new JsonObject()
+                .put(HOST, safeConfig.getString(HOST, null))
+                .put(THEME_PLATFORM, safeConfig.getString(THEME_PLATFORM, "default"));
         if ("angular".equals(view)) {
             renderView(request, new JsonObject(), "formulaire.html", null);
         } else {
-            renderView(request, new JsonObject(), "index.html", null);
+            renderView(request, params, "index.html", null);
         }
         eventStore.createAndStoreEvent(ACCESS.name(), request);
     }
@@ -57,7 +64,8 @@ public class FormulaireController extends ControllerHelper {
 
         JsonObject nodePdfGenerator = safeConfig.getJsonObject(NODE_PDF_GENERATOR, null);
         if (nodePdfGenerator != null) {
-            if (nodePdfGenerator.getString(AUTH, null) != null) nodePdfGenerator.put(AUTH, "**********");
+            if (nodePdfGenerator.getString(AUTH, null) != null)
+                nodePdfGenerator.put(AUTH, "**********");
         }
 
         renderJson(request, safeConfig);
