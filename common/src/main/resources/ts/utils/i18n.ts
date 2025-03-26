46ad6025a671cd95cc15c7ad109c45500a594378
Thomas Sevagen <thomas.sevagen@cgi.com>
2025-03-26 12:08:50 +0100

    fix(i18n): correct parameter placeholder syntax in translation utility

--- common/src/main/resources/ts/utils/i18n.ts
+++ common/src/main/resources/ts/utils/i18n.ts
@@ -4,7 +4,7 @@ export class I18nUtils {
     static getWithParams = (key: string, params: string[]) : string => {
         let finalI18n = idiom.translate(key);
         for (let i = 0; i < params.length; i++) {
-            finalI18n = finalI18n.replace(`{${i}}`, params[i]);
+            finalI18n = finalI18n.replace(`{{${i}}}`, params[i]);
         }
         return finalI18n;
     };
