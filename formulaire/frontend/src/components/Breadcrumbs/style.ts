f6a977b0e20e73ca5c2ee438fc5aa8c8e637961e
Quentin Périé <perieq@hotmail.fr>
2025-04-02 10:50:00 +0200

    fix(): fix minors errors

--- formulaire/frontend/src/components/Breadcrumbs/style.ts
+++ formulaire/frontend/src/components/Breadcrumbs/style.ts
@@ -1,6 +1,14 @@
-import { spaceBetweenBoxStyle } from "~/styles/boxStyles";
+import { styled, Typography } from "@cgi-learning-hub/ui";
+import { BreadCrumbItemWrapperProps } from "./types";
 
-export const headerStyle = {
-  ...spaceBetweenBoxStyle,
-  padding: "3rem",
-};
+export const BreadCrumbItemWrapper = styled(Typography, {
+  shouldForwardProp: (prop) => prop !== "textColor" && prop !== "isHeader" && prop !== "hasSeparator",
+})<BreadCrumbItemWrapperProps>(({ textColor, isHeader, hasSeparator }) => ({
+  color: textColor,
+  fontSize: isHeader ? "2.4rem" : "2rem",
+  marginLeft: hasSeparator ? "-1rem" : "",
+  "&:hover": {
+    cursor: "pointer",
+    textDecoration: "underline",
+  }
+}));
