8466e0e0baee042cc2d0c66ef169b6bfbf1471b7
Quentin Périé <perieq@hotmail.fr>
2025-04-02 11:28:15 +0200

    fix(): fix minors errors (#451)
    * fix(): fix minors errors
    
    * lint
    
    * revert unwanted changes

--- formulaire/frontend/src/components/Breadcrumbs/index.tsx
+++ formulaire/frontend/src/components/Breadcrumbs/index.tsx
@@ -2,19 +2,19 @@ import { FC } from "react";
 
 import { FormBreadcrumbsProps } from "./types";
 import { Breadcrumbs } from "@mui/material";
-import { Box, Link, Typography } from "@cgi-learning-hub/ui";
+import { Box, Link } from "@cgi-learning-hub/ui";
 import { FormsIcon } from "../SVG/FormsIcon";
 import { FORM_COLOR, GREY_DARKER_COLOR, TEXT_PRIMARY_COLOR } from "~/core/style/colors";
 import { FORMULAIRE } from "~/core/constants";
+import { BreadCrumbItemWrapper } from "./style";
+import { useNavigate } from "react-router-dom";
 
-export const FormBreadcrumbs: FC<FormBreadcrumbsProps> = ({
-  stringItems,
-  separator = null,
-  isHeader = false,
-  displaySeparator = false,
-}) => {
+export const FormBreadcrumbs: FC<FormBreadcrumbsProps> = ({ stringItems, separator = null, isHeader = false }) => {
+  const textColor = isHeader ? FORM_COLOR : TEXT_PRIMARY_COLOR;
+  const navigate = useNavigate();
+
   return (
-    <Breadcrumbs separator={displaySeparator && (separator ?? null)} maxItems={3}>
+    <Breadcrumbs separator={separator} maxItems={3}>
       {isHeader ? (
         <Link underline="hover" color={FORM_COLOR} href={`/${FORMULAIRE}`}>
           <FormsIcon height="5rem" />
@@ -25,14 +25,17 @@ export const FormBreadcrumbs: FC<FormBreadcrumbsProps> = ({
         </Box>
       )}
       {stringItems.map((stringItem) => (
-        <Typography
+        <BreadCrumbItemWrapper
           key={stringItem}
-          color={isHeader ? FORM_COLOR : TEXT_PRIMARY_COLOR}
-          fontSize={isHeader ? "2.4rem" : "2rem"}
-          marginLeft={separator === null || !displaySeparator ? "-1rem" : ""}
+          textColor={textColor}
+          isHeader={isHeader}
+          hasSeparator={!separator}
+          onClick={() => {
+            navigate("/");
+          }}
         >
           {stringItem}
-        </Typography>
+        </BreadCrumbItemWrapper>
       ))}
     </Breadcrumbs>
   );
