6a70105a42cbf6d09900ac0bf021edabdf3fe2af
Alex Delaveau <57497688+Alex-Delaveau@users.noreply.github.com>
2025-04-03 16:12:17 +0200

    feat(dnd): #FOR-889 add dnd on form homepage (#448)

--- formulaire/frontend/src/containers/HomeMainFolders/types.ts
+++ formulaire/frontend/src/containers/HomeMainFolders/types.ts
@@ -1,5 +1,7 @@
 import { Folder } from "~/core/models/folder/types";
+import { ActiveDragItemProps } from "~/hook/dnd-hooks/types";
 
 export interface HomeMainFolderProps {
+  activeItem: ActiveDragItemProps;
   folders: Folder[];
 }
--- formulaire/frontend/src/containers/HomeMainLayout/index.tsx
+++ formulaire/frontend/src/containers/HomeMainLayout/index.tsx
@@ -1,6 +1,6 @@
-import { FC, useCallback, useMemo, useState } from "react";
+import { FC, useMemo, useState } from "react";
 import { Box, SearchInput } from "@cgi-learning-hub/ui";
-import { Switch, useTheme } from "@mui/material";
+import { Switch, Typography, useTheme } from "@mui/material";
 import { FormBreadcrumbs } from "~/components/Breadcrumbs";
 
 import { useHome } from "~/providers/HomeProvider";
@@ -17,68 +17,47 @@ import { HomeMainForms } from "../HomeMainForms";
 import { FORMULAIRE } from "~/core/constants";
 import { OrganizeFilter } from "~/components/OrganizeFilter";
 import { ChipProps, MenuItemProps } from "~/components/OrganizeFilter/types";
-import { chipData, getEmptyStateDescription, menuItemData } from "./utils";
-import { MenuItemState } from "~/components/OrganizeFilter/enum";
+import { chipData, getDragCursorStyle, getEmptyStateDescription, menuItemData } from "./utils";
 import { ResourcesEmptyState } from "~/components/SVG/RessourcesEmptyState";
 import { useEdificeClient } from "@edifice.io/react";
+import { DndContext, DragOverlay, pointerWithin } from "@dnd-kit/core";
+import { useFormFolderDnd } from "~/hook/dnd-hooks/useFormFolderDnd";
+import { isDraggedItemFolder, isDraggedItemForm } from "~/hook/dnd-hooks/utils";
+import { FolderPreview } from "~/components/FolderPreview";
+import { buildFlatFolderTree } from "../HomeSidebar/utils";
+import { DroppableTreeItem } from "~/components/DroppableTreeItem";
+import { useSearchAndOrganize } from "./useSearchAndOrganize";
+import { Form } from "~/core/models/form/types";
+import { Folder } from "~/core/models/folder/types";
+import { FormPreview } from "~/components/FormPreview";
 
 export const HomeMainLayout: FC = () => {
-  const { folders, forms, currentFolder } = useHome();
+  const { folders, forms, currentFolder, selectedFolders, selectedForms, setSelectedFolders, setSelectedForms } =
+    useHome();
   const theme = useTheme();
   const { t } = useTranslation(FORMULAIRE);
   const { user } = useEdificeClient();
   const [selectedChips, setSelectedChips] = useState<ChipProps[]>([]);
   const [selectedMenuItem, setSelectedMenuItem] = useState<MenuItemProps>();
-  const [searchText, setSearchText] = useState("");
   const userId = user?.userId;
+  const { handleDragStart, handleDragOver, handleDragEnd, handleDragCancel, sensors, activeDragItem, isValidDrop } =
+    useFormFolderDnd(selectedFolders, selectedForms, setSelectedFolders, setSelectedForms, currentFolder);
 
-  const handleSearch = useCallback((searchValue: string) => {
-    setSearchText(searchValue);
-  }, []);
+  const flatTreeViewItems = buildFlatFolderTree(folders);
 
-  const filteredFolders = useMemo(() => {
-    return folders.filter((folder) => folder.parent_id === currentFolder.id);
-  }, [folders, currentFolder.id]);
+  const { handleSearch, filteredFolders, hasFilteredFolders, filteredForms, hasFilteredForms } = useSearchAndOrganize(
+    folders,
+    forms,
+    currentFolder,
+    userId,
+    selectedChips,
+    selectedMenuItem,
+  );
 
-  const filteredForms = useMemo(() => {
-    const initialFiltered = forms.filter((form) => {
-      const isCurrentUser = form.owner_id === userId;
-
-      if (currentFolder.id === 2) {
-        return form.collab && !isCurrentUser;
-      }
-
-      return form.folder_id === currentFolder.id && isCurrentUser;
-    });
-
-    const searchFiltered = searchText.trim()
-      ? initialFiltered.filter((form) => {
-          const formattedSearchText = searchText.trim().toLowerCase();
-          return (
-            form.title.toLowerCase().includes(formattedSearchText) ||
-            form.owner_name.toLowerCase().includes(formattedSearchText)
-          );
-        })
-      : initialFiltered;
-
-    const chipFiltered = selectedChips.length
-      ? searchFiltered.filter((form) => selectedChips.every((chip) => chip.filterFn(form)))
-      : searchFiltered;
-
-    if (selectedMenuItem) {
-      const isAscending = selectedMenuItem.state === MenuItemState.ASCENDING;
-      return [...chipFiltered].sort((a, b) => selectedMenuItem.sortFn(a, b, isAscending));
-    }
-
-    return chipFiltered;
-  }, [forms, searchText, currentFolder.id, selectedChips, selectedMenuItem]);
-
-  const hasFilteredFolders = useMemo(() => !!filteredFolders.length, [filteredFolders]);
-  const hasFilteredForms = useMemo(() => !!filteredForms.length, [filteredForms]);
   const breadcrumbsText = useMemo(() => (currentFolder?.name ? [currentFolder.name] : []), [currentFolder?.name]);
 
   return (
-    <Box sx={mainContentInnerStyle}>
+    <Box sx={{ ...mainContentInnerStyle, ...getDragCursorStyle(activeDragItem, isValidDrop) }}>
       <Box sx={searchStyle}>
         <SearchInput
           placeholder={t("formulaire.search.placeholder")}
@@ -98,18 +77,37 @@ export const HomeMainLayout: FC = () => {
         <FormBreadcrumbs stringItems={breadcrumbsText} />
         <Switch />
       </Box>
-      {(hasFilteredFolders || hasFilteredForms) && (
-        <Box sx={resourceContainerStyle}>
-          {hasFilteredFolders && <HomeMainFolders folders={filteredFolders} />}
-          {hasFilteredForms && <HomeMainForms forms={filteredForms} />}
-        </Box>
-      )}
-      {!hasFilteredFolders && !hasFilteredForms && (
-        <Box sx={emptyStateWrapperStyle}>
-          <ResourcesEmptyState fill={theme.palette.primary.main} />
-          <p>{t(getEmptyStateDescription(currentFolder))}</p>
-        </Box>
-      )}
+      <DndContext
+        sensors={sensors}
+        collisionDetection={pointerWithin}
+        onDragStart={handleDragStart}
+        onDragOver={handleDragOver}
+        onDragEnd={handleDragEnd}
+        onDragCancel={handleDragCancel}
+      >
+        {(hasFilteredFolders || hasFilteredForms) && (
+          <Box sx={resourceContainerStyle}>
+            {hasFilteredFolders && <HomeMainFolders folders={filteredFolders} activeItem={activeDragItem} />}
+            {hasFilteredForms && <HomeMainForms forms={filteredForms} activeItem={activeDragItem} />}
+          </Box>
+        )}
+        {flatTreeViewItems.map((item) => (
+          <DroppableTreeItem key={item.internalId} treeItemId={item.internalId} />
+        ))}
+        <DragOverlay>
+          {isDraggedItemForm(activeDragItem) ? (
+            <FormPreview form={activeDragItem.data as Form} />
+          ) : isDraggedItemFolder(activeDragItem) ? (
+            <FolderPreview folder={activeDragItem.data as Folder} />
+          ) : null}
+        </DragOverlay>
+        {!hasFilteredFolders && !hasFilteredForms && (
+          <Box sx={emptyStateWrapperStyle}>
+            <ResourcesEmptyState fill={theme.palette.primary.main} />
+            <Typography>{t(getEmptyStateDescription(currentFolder))}</Typography>
+          </Box>
+        )}
+      </DndContext>
     </Box>
   );
 };
