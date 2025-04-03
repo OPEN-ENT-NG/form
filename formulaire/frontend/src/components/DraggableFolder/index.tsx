ed9d15f9f026cf268982d8bba528c9bc374bf6b6
Alex Delaveau <57497688+Alex-Delaveau@users.noreply.github.com>
2025-04-03 16:12:17 +0200

    feat(dnd): #FOR-889 add dnd on form homepage (#448)

---
+++ formulaire/frontend/src/components/DraggableFolder/index.tsx
@@ -0,0 +1,69 @@
+import { useDndMonitor, useDraggable, useDroppable } from "@dnd-kit/core";
+import { FC, useState } from "react";
+import { DraggableType } from "~/core/enums";
+
+import { FolderCard } from "@cgi-learning-hub/ui";
+import { DraggableFolderProps } from "./types";
+import { StyledDraggableFolder } from "./style";
+
+export const DraggableFolder: FC<DraggableFolderProps> = ({
+  folder,
+  dragActive = false,
+  onSelect,
+  onClick,
+  isSelected,
+  getFolderSubtitle,
+}) => {
+  const [isOvered, setIsOvered] = useState(false);
+
+  const {
+    attributes,
+    listeners,
+    setNodeRef: setDraggableNodeRef,
+  } = useDraggable({
+    id: `draggable-folder-${folder.id}`,
+    data: { type: DraggableType.FOLDER, folder },
+  });
+
+  const { setNodeRef: setDroppableNodeRef } = useDroppable({
+    id: `droppable-folder-${folder.id}`,
+    data: { type: DraggableType.FOLDER, folder },
+  });
+
+  const setNodeRef = (node: HTMLElement | null) => {
+    setDraggableNodeRef(node);
+    setDroppableNodeRef(node);
+  };
+
+  useDndMonitor({
+    onDragOver(event) {
+      const isOvered =
+        !!event.over &&
+        event.over.id === `droppable-folder-${folder.id}` &&
+        !!event.active.data.current &&
+        [DraggableType.FORM, DraggableType.FOLDER].includes(event.active.data.current.type);
+      setIsOvered(isOvered);
+    },
+    onDragEnd() {
+      setIsOvered(false);
+    },
+    onDragCancel() {
+      setIsOvered(false);
+    },
+  });
+
+  return (
+    <StyledDraggableFolder ref={setNodeRef} {...attributes} {...listeners} dragActive={dragActive} isOvered={isOvered}>
+      <FolderCard
+        key={folder.id}
+        width="30rem"
+        title={folder.name}
+        subtitle={getFolderSubtitle(folder)}
+        onSelect={() => onSelect(folder)}
+        onClick={() => onClick(folder)}
+        isSelected={isSelected(folder)}
+        iconSize="3.2rem"
+      />
+    </StyledDraggableFolder>
+  );
+};
---
+++ formulaire/frontend/src/core/style/dndStyle.ts
@@ -0,0 +1,20 @@
+import { SxProps } from "@mui/material";
+
+export const cardPreviewStyle: SxProps = {
+  opacity: 0.5,
+  width: "30rem",
+};
+
+export const droppableStyle: SxProps = {
+  boxSizing: "border-box",
+  border: "2px solid transparent",
+  borderRadius: ".8rem",
+};
+
+export const overedStyle: SxProps = {
+  border: "2px solid #e4e4e4",
+};
+
+export const dragActiveStyle: SxProps = {
+  opacity: 0.5,
+};
