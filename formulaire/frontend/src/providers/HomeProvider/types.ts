92870e54fc65b08b502e0cf3b2921e995110eda7
Thomas Sevagen <thomas.sevagen@cgi.com>
2025-04-08 15:09:51 +0200

    feat(table): [#FOR-883] add table view and toggleView (#450) (#456)
    * WIP
    
    * WIP
    
    * feat(table): [#FOR-883] add table view and toggleView
    
    * feat(table): [#FOR-883] add table view
    
    * linter + improvements
    
    * fix color var
    
    Co-authored-by: Quentin Périé <perieq@hotmail.fr>

--- formulaire/frontend/src/providers/HomeProvider/types.ts
+++ formulaire/frontend/src/providers/HomeProvider/types.ts
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import { Folder } from "~/core/models/folder/types";
 import { HomeTabState } from "./enums";
 import { Form } from "~/core/models/form/types";
+import { ViewMode } from "~/components/SwitchView/enums";
 
 export interface HomeProviderProps {
   children: ReactNode;
@@ -12,6 +13,8 @@ export type HomeProviderContextType = {
   setCurrentFolder: (value: Folder) => void;
   tab: HomeTabState;
   toggleTab: (tab: HomeTabState) => void;
+  tabViewPref: HomeTabViewPref;
+  toggleTagViewPref: (viewMode: ViewMode) => void;
   folders: Folder[];
   setFolders: (value: Folder[]) => void;
   forms: Form[];
@@ -23,3 +26,8 @@ export type HomeProviderContextType = {
   isToasterOpen: boolean;
   resetSelected: () => void;
 };
+
+export interface HomeTabViewPref {
+  [HomeTabState.FORMS]: ViewMode;
+  [HomeTabState.RESPONSES]: ViewMode;
+}
