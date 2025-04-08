8eb9bd566ce82abb26e7575ad1ce7511ddf0e1e7
Alex Delaveau <57497688+Alex-Delaveau@users.noreply.github.com>
2025-04-08 10:30:56 +0200

    feat(move-modal): #FOR-882 add move form or folder modal (#452)

---
+++ formulaire/frontend/src/containers/MoveFolderModal/style.ts
@@ -0,0 +1,17 @@
+import { SxProps } from "@cgi-learning-hub/ui";
+
+export const moveMainTextStyle: SxProps = {
+  display: "flex",
+  flexDirection: "row",
+};
+
+export const treeviewTextStyle: SxProps = {
+  fontStyle: "italic",
+  marginTop: "3rem",
+  marginBottom: "1rem",
+};
+
+export const treeviewContainerStyle: SxProps = {
+  height: "25rem",
+  width: "70%",
+};
