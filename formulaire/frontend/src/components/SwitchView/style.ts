29ffafa5cb53c5b93b752ba7ddd9f51f2f8c005a
Thomas Sevagen <thomas.sevagen@cgi.com>
2025-04-08 15:09:51 +0200

    feat(table): [#FOR-883] add table view and toggleView (#450) (#456)
    * WIP
    
    * WIP
    
    * feat(table): [#FOR-883] add table view and toggleView
    
    * feat(table): [#FOR-883] add table view
    
    * linter + improvements
    
    * fix color var
    
    Co-authored-by: Quentin Périé <perieq@hotmail.fr>

---
+++ formulaire/frontend/src/components/SwitchView/style.ts
@@ -0,0 +1,27 @@
+import { PRIMARY_LIGHT_COLOR } from "~/core/style/colors";
+import {
+  CSS_COMMON_WHITE_COLOR,
+  CSS_PRIMARY_LIGHTER_COLOR,
+  CSS_PRIMARY_MAIN_COLOR,
+  CSS_TEXT_SECONDARY_COLOR,
+} from "~/core/style/cssColors";
+
+export const switchViewStyle = {
+  borderColor: PRIMARY_LIGHT_COLOR,
+};
+
+export const switchViewItemStyle = {
+  "&.Mui-selected": {
+    backgroundColor: CSS_PRIMARY_MAIN_COLOR,
+    color: CSS_COMMON_WHITE_COLOR,
+  },
+  "&:hover": {
+    backgroundColor: CSS_PRIMARY_LIGHTER_COLOR,
+    color: CSS_TEXT_SECONDARY_COLOR,
+  },
+  "&.Mui-selected:hover": {
+    backgroundColor: CSS_PRIMARY_MAIN_COLOR,
+    color: CSS_COMMON_WHITE_COLOR,
+  },
+  color: CSS_TEXT_SECONDARY_COLOR,
+};
