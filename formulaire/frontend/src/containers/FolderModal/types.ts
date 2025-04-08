e577cc1f4b72d1700919a7b8fe6d757a9f54fd75
Thomas Sevagen <thomas.sevagen@cgi.com>
2025-04-08 18:06:50 +0200

    chore(lint): add new linter and refac app to feat with (#457)
    * feat(eslint): newlinter +update package
    
    update files with new linter
    
    refactor: rename ModalProps to IModalProps and update related usages; adjust modal toggle types
    
    feat(dependencies): add @testing-library/jest-dom for improved testing support
    
    * refac(types): rename types for consistency across components
    
    * feat(modals): update modal types for consistency and clarity

--- formulaire/frontend/src/containers/FolderModal/types.ts
+++ formulaire/frontend/src/containers/FolderModal/types.ts
@@ -1,5 +1,9 @@
-import { ModalProps } from "~/types";
-import { FOLDER_MODAL_MODE } from "./enum";
-export interface FolderModalProps extends ModalProps {
-  mode: FOLDER_MODAL_MODE;
+import { IModalProps } from "~/types";
+export enum FolderModalMode {
+  CREATE = "CREATE",
+  RENAME = "RENAME",
+}
+
+export interface IFolderModalProps extends IModalProps {
+  mode: FolderModalMode;
 }
