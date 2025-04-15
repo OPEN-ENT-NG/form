9cbc6bd7d5eb4f7597e89b68593634c5e9157d91
Alex Delaveau <57497688+Alex-Delaveau@users.noreply.github.com>
2025-04-15 09:34:32 +0200

    feat(share): FOR-878 add share modal (#458)
    * feat(share): FOR-878 add share modal
    
    * revert visual change on share modal
    
    * add toaster rigths condition to show buttons
    
    * fix rebase from dev-react
    
    * Linter
    
    * fix misc
    
    * fix log

---
+++ formulaire/frontend/src/common/ShareModal/ShareBookmark.tsx
@@ -0,0 +1,49 @@
+import { Ref } from "react";
+
+import { FormControl, Button } from "@edifice.io/react";
+import { IconSave } from "@edifice.io/react/icons";
+import { useTranslation } from "react-i18next";
+
+import { BookmarkProps } from "./hooks/useShareBookmark";
+import { Box } from "@cgi-learning-hub/ui";
+export const ShareBookmark = ({
+  bookmark,
+  refBookmark,
+  onBookmarkChange,
+  onSave,
+}: {
+  bookmark: BookmarkProps;
+  refBookmark: Ref<HTMLInputElement>;
+  onBookmarkChange: () => void;
+  onSave: () => void;
+}) => {
+  const { t } = useTranslation();
+
+  return (
+    <Box className="mt-16">
+      <FormControl id="bookmarkName" className="d-flex flex-wrap align-items-center gap-16">
+        <Box className="flex-fill">
+          <FormControl.Input
+            key={bookmark.id}
+            ref={refBookmark}
+            onChange={onBookmarkChange}
+            placeholder={t("explorer.modal.share.sharebookmark.placeholder")}
+            size="sm"
+            type="text"
+          />
+        </Box>
+        <Button
+          type="button"
+          color="primary"
+          variant="ghost"
+          disabled={bookmark.name.length === 0}
+          leftIcon={<IconSave />}
+          onClick={onSave}
+          className="text-nowrap"
+        >
+          {t("explorer.modal.share.sharebookmark.save")}
+        </Button>
+      </FormControl>
+    </Box>
+  );
+};
