77f3cc0680224c36bc657b76062965e73a2227ed
Quentin Périé <perieq@hotmail.fr>
2025-04-22 09:38:36 +0200

    fix(): improve design and behaviors (#461)
    * fix(): improve design
    
    * improve Enter key pressed handling
    
    * linter
    
    * lint
    
    * use const value for a color

--- formulaire/frontend/src/providers/HomeProvider/utils.ts
+++ formulaire/frontend/src/providers/HomeProvider/utils.ts
@@ -5,6 +5,9 @@ import { FORMULAIRE } from "~/core/constants";
 import { HomeTabState, RootFolderIds } from "./enums";
 import { ViewMode } from "~/components/SwitchView/enums";
 import { IHomeTabViewPref } from "./types";
+import FolderIcon from "@mui/icons-material/Folder";
+import ShareIcon from "@mui/icons-material/Share";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export const useRootFolders = (): IFolder[] => {
   const { t } = useTranslation(FORMULAIRE);
@@ -15,6 +18,7 @@ export const useRootFolders = (): IFolder[] => {
         id: RootFolderIds.FOLDER_MY_FORMS_ID,
         parent_id: null,
         name: t("formulaire.forms.mine"),
+        icon: FolderIcon,
         user_id: "",
         nb_folder_children: 0,
         nb_form_children: 0,
@@ -25,6 +29,7 @@ export const useRootFolders = (): IFolder[] => {
         id: RootFolderIds.FOLDER_SHARED_FORMS_ID,
         parent_id: null,
         name: t("formulaire.forms.shared"),
+        icon: ShareIcon,
         user_id: "",
         nb_folder_children: 0,
         nb_form_children: 0,
@@ -35,6 +40,7 @@ export const useRootFolders = (): IFolder[] => {
         id: RootFolderIds.FOLDER_ARCHIVED_ID,
         parent_id: null,
         name: t("formulaire.forms.archived"),
+        icon: DeleteIcon,
         user_id: "",
         nb_folder_children: 0,
         nb_form_children: 0,
