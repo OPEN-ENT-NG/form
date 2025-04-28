18857e2fe360af012bc8f564863efbf4c4257f48
Quentin Périé <perieq@hotmail.fr>
2025-04-28 10:13:05 +0200

    fix(tab): display tabs according to workflow rights (#473)
    * fix(tab): display tabs according to workflow rights
    
    * fix(routing): fix display default home page according to user rights
    
    * oups
    
    * fix syntax
    
    * retours PR

--- formulaire/frontend/src/core/rights.ts
+++ formulaire/frontend/src/core/rights.ts
@@ -1,3 +1,41 @@
-export const workflowRights = {
-  access: "fr.openent.formulaire_public.controllers.FormulairePublicController|initAccessRight",
+// Types
+export interface IWorkflowRights {
+  access: string;
+  creation: string;
+  response: string;
+  rgpd: string;
+  creationPublic: string;
+}
+
+export interface IUserWorkflowRights {
+  [WorkflowRights.ACCESS]: boolean;
+  [WorkflowRights.CREATION]: boolean;
+  [WorkflowRights.RESPONSE]: boolean;
+  [WorkflowRights.RGPD]: boolean;
+  [WorkflowRights.CREATION_PUBLIC]: boolean;
+}
+
+// Enums
+export enum WorkflowRights {
+  ACCESS = "ACCESS",
+  CREATION = "CREATION",
+  RESPONSE = "RESPONSE",
+  RGPD = "RGPD",
+  CREATION_PUBLIC = "CREATION_PUBLIC",
+}
+
+// Const
+export const workflowRights: IWorkflowRights = {
+  access: "fr.openent.formulaire.controllers.FormulaireController|render",
+  creation: "fr.openent.formulaire.controllers.FormController|initCreationRight",
+  response: "fr.openent.formulaire.controllers.FormController|initResponseRight",
+  rgpd: "fr.openent.formulaire.controllers.DelegateController|initRGPDRight",
+  creationPublic: "fr.openent.formulaire.controllers.FormController|initCreationPublicRight",
+};
+
+export const sharingRights = {
+  read: "fr-openent-formulaire-controllers-FormController|initReadResourceRight",
+  contrib: "fr-openent-formulaire-controllers-FormController|initContribResourceRight",
+  manager: "fr-openent-formulaire-controllers-FormController|initManagerResourceRight",
+  responder: "fr-openent-formulaire-controllers-FormController|initResponderResourceRight",
 };
