d0028137474eec7470b5390f261e237d25f74774
Quentin Périé <perieq@hotmail.fr>
2025-05-05 11:52:15 +0200

    fix(): add public link in share modal (#476)
    * WIP
    
    * fix(): add public link in share modal

--- formulaire/frontend/src/common/ShareModal/utils/showShareRightLine.ts
+++
@@ -1,4 +0,0 @@
-import { ShareRight } from "@edifice.io/client";
-
-export const showShareRightLine = (shareRight: ShareRight, showBookmarkMembers: boolean): boolean =>
-  (shareRight.isBookmarkMember && showBookmarkMembers) || !shareRight.isBookmarkMember;
