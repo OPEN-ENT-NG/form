ea1561cdd8bc605999ca8ad13d7ae2b00dbcbe99
Alex Delaveau <alex26delaveau@gmail.com>
2025-05-07 14:58:54 +0200

    fix(return): #FOR-954 fix tabs moving between tab

---
+++ formulaire/frontend/src/components/AnswerMainLayout/style.ts
@@ -0,0 +1,13 @@
+import { SxProps, Theme } from "@mui/material";
+import { spaceBetweenBoxStyle } from "~/core/style/boxStyles";
+
+export const myAnswerHeader: SxProps<Theme> = {
+  ...spaceBetweenBoxStyle,
+  gap: "2rem",
+  height: "3.5rem",
+  padding: "0 5rem 0 0",
+};
+
+export const tabStyle: SxProps<Theme> = {
+  padding: "0 0 0 2rem",
+};
