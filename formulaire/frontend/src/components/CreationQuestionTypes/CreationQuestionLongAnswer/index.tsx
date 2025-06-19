0a00ddf4914deeb383861b463f9fe18d2f503241
Alex Delaveau <57497688+Alex-Delaveau@users.noreply.github.com>
2025-06-19 15:55:41 +0200

    feat(longAnswer): #FOR-900 add long answer question type (#490)

---
+++ formulaire/frontend/src/components/CreationQuestionTypes/CreationQuestionLongAnswer/index.tsx
@@ -0,0 +1,26 @@
+import { FC } from "react";
+import { Box, TextField } from "@cgi-learning-hub/ui";
+import { ComponentVariant } from "~/core/style/themeProps";
+import { useTranslation } from "react-i18next";
+import { FORMULAIRE } from "~/core/constants";
+import { longAnswerStyle } from "./style";
+
+export const CreationQuestionLongAnswer: FC = () => {
+  const { t } = useTranslation(FORMULAIRE);
+
+  return (
+    <Box>
+      <TextField
+        variant={ComponentVariant.OUTLINED}
+        sx={longAnswerStyle}
+        fullWidth
+        placeholder={t("formulaire.question.type.LONGANSWER")}
+        slotProps={{
+          input: {
+            readOnly: true,
+          },
+        }}
+      />
+    </Box>
+  );
+};
