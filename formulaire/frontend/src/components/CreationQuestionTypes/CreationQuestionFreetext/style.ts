b2fcda37d074f533c57f9e5a91aad0b0c653c71e
Quentin Périé <perieq@hotmail.fr>
2025-06-27 17:27:16 +0200

    feat(freetext): [#FOR-896] add freetext question type (#491)
    * feat(freetext): [#FOR-896] add freetext question type
    
    * fix focus setting when clicking in editor
    
    * lint and format

---
+++ formulaire/frontend/src/components/CreationQuestionTypes/CreationQuestionFreetext/style.ts
@@ -0,0 +1,15 @@
+import { styled, Box } from "@cgi-learning-hub/ui";
+import { IEditorWrapperProps } from "./types";
+
+export const StyledEditorWrapper = styled(Box, {
+  shouldForwardProp: (prop) => prop !== "isCurrentEditingElement",
+})<IEditorWrapperProps>(({ isCurrentEditingElement }) => {
+  if (!isCurrentEditingElement) return { userSelect: "none", pointerEvents: "none" };
+  return {
+    "> :first-child": {
+      "> :nth-child(2)": {
+        minHeight: "200px",
+      },
+    },
+  };
+});
