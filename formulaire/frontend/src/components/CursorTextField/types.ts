312b59c1ff85f77a36efb2537e72774af00ee780
Alex Delaveau <57497688+Alex-Delaveau@users.noreply.github.com>
2025-07-17 11:06:24 +0200

    fix(): fix various problems (getElement, delete, cursorQuestion,...) (#501)
    * fix(delete): fix delete section not reseting currentEditingElement
    
    * fix(type): fix get element by id not type robust
    
    * fix(type): fix finding formELement by id without type
    
    * fix(undo): undo on new element now delete it
    
    * fix(choices): fix error when creating question with choices
    
    * wip
    
    * fix croise dev
    
    * Fix editor issues
    
    * fix lint
    
    * Fix magic Strings
    
    * fix returns

--- formulaire/frontend/src/components/CursorTextField/types.ts
+++ formulaire/frontend/src/components/CursorTextField/types.ts
@@ -1,8 +1,10 @@
+import { CursorProp } from "../CreationQuestionTypes/CreationQuestionCursor/enums";
 import { CursorTextFieldType } from "./enums";
 
 export interface ICursorTextFieldProps {
   type: CursorTextFieldType;
   isCurrentEditingElement: boolean;
+  propName: CursorProp;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   inputValue: string | number;
   stepValue?: number;
