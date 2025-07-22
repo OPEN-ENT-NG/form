6406dbf9cf9a86472cc1ef4bfd2b4b6f16883e9a
Alex Delaveau <alex26delaveau@gmail.com>
2025-07-22 11:57:37 +0200

    feat(choice): #FOR-910 add choice wrapper

---
+++ formulaire/frontend/src/components/QuestionChoicesUpDownButtons/style.ts
@@ -0,0 +1,14 @@
+import { SxProps } from "@mui/material";
+
+export const iconStyle: SxProps = {
+  fontSize: "2.4rem",
+  borderRadius: "50%",
+};
+
+export const arrowWrapperStyle: SxProps = {
+  width: 28,
+  height: 28,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+};
