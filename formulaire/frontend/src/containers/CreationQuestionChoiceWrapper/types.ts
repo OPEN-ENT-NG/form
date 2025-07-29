f74fc894051b4cbf9feae1b07711afd9b63066af
Alex Delaveau <57497688+Alex-Delaveau@users.noreply.github.com>
2025-07-29 10:03:26 +0200

    feat(choice): #FOR-909 add question choice (#503)

--- formulaire/frontend/src/containers/CreationQuestionChoiceWrapper/types.ts
+++ formulaire/frontend/src/containers/CreationQuestionChoiceWrapper/types.ts
@@ -1,5 +1,7 @@
+import { CreationQuestionChoiceType } from "~/components/CreationQuestionTypes/CreationQuestionChoice/enum";
 import { IQuestion } from "~/core/models/question/types";
 
 export interface ICreationQuestionChoiceWrapperProps {
   question: IQuestion;
+  type: CreationQuestionChoiceType;
 }
