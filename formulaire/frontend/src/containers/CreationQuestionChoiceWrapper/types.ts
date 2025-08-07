8f8e7354c74578e1a34aff6105ebb1059c844cc7
Alex Delaveau <57497688+Alex-Delaveau@users.noreply.github.com>
2025-08-07 00:27:11 -0700

    feat(choice): #FOR-899 add multiple and radio choice (#525)

--- formulaire/frontend/src/containers/CreationQuestionChoiceWrapper/types.ts
+++ formulaire/frontend/src/containers/CreationQuestionChoiceWrapper/types.ts
@@ -1,7 +1,11 @@
-import { CreationQuestionChoiceType } from "~/components/CreationQuestionTypes/CreationQuestionChoice/enum";
+import { QuestionTypes } from "~/core/models/question/enum";
 import { IQuestion } from "~/core/models/question/types";
 
 export interface ICreationQuestionChoiceWrapperProps {
   question: IQuestion;
-  type: CreationQuestionChoiceType;
+  type: QuestionTypes;
+}
+
+export interface INewChoiceWrapperProps {
+  hasImage: boolean;
 }
