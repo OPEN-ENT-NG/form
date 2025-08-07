a908585affc61b531fcce138f8ad2e8f625cea6c
Alex Delaveau <57497688+Alex-Delaveau@users.noreply.github.com>
2025-08-07 07:23:44 -0700

    feat(choice): #FOR-907 add conditional choice target next element (#527)

---
+++ formulaire/frontend/src/components/CreationQuestionChoiceConditional/types.ts
@@ -0,0 +1,13 @@
+import { FormElementType } from "~/core/models/formElement/enum";
+import { IQuestion, IQuestionChoice } from "~/core/models/question/types";
+
+export interface ICreationQuestionChoiceConditionalProps {
+  question: IQuestion;
+  choice: IQuestionChoice;
+  choiceIndex: number;
+  updateChoiceNextFormElement: (
+    index: number | null,
+    nextFormElementId: number | undefined,
+    nextFormElementType: FormElementType | undefined,
+  ) => void;
+}
