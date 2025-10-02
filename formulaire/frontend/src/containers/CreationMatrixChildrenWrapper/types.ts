b09de4932c44c460b4eed433f482631dad40e9df
Quentin Périé <perieq@hotmail.fr>
2025-10-02 17:43:48 +0200

    remove useless code

--- formulaire/frontend/src/containers/CreationMatrixChildrenWrapper/types.ts
+++ formulaire/frontend/src/containers/CreationMatrixChildrenWrapper/types.ts
@@ -3,7 +3,3 @@ import { IQuestion } from "~/core/models/question/types";
 export interface ICreationMatrixChildrenWrapperProps {
   question: IQuestion;
 }
-
-export interface INewChoiceWrapperProps {
-  hasImage: boolean;
-}
