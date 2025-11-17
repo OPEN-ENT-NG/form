31117df9a4e846cd69df33eb2222e4404a97188a
Quentin Périé <perieq@hotmail.fr>
2025-11-17 18:41:14 +0100

    feat(respond): [#FOR-920] init response wrappers for questions and sections

---
+++ formulaire/frontend/src/containers/RespondSectionWrapper/style.ts
@@ -0,0 +1,16 @@
+import { SxProps, Theme } from "@cgi-learning-hub/ui";
+
+export const questionStackStyle: SxProps<Theme> = {
+  width: "100%",
+  display: "flex",
+  flexDirection: "column",
+  borderRadius: 1,
+  marginBottom: 2,
+  padding: "3rem",
+  boxShadow: "1px 4px 5px 2px rgba(0, 0, 0, 0.1)",
+  rowGap: "2rem",
+};
+
+export const mandatoryTitleStyle: SxProps<Theme> = {
+  marginLeft: "0.5rem",
+};
