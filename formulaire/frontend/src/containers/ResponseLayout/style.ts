403023a3d66da72ab5e619e167c7798f87891787
Quentin Périé <perieq@hotmail.fr>
2025-11-19 12:52:27 +0100

    feat(respond): [#FOR-920] init response wrappers for questions and sections (#541)

--- formulaire/frontend/src/containers/ResponseLayout/style.ts
+++ formulaire/frontend/src/containers/ResponseLayout/style.ts
@@ -1,11 +1,5 @@
 import { Box, styled, SxProps, Theme } from "@cgi-learning-hub/ui";
-import {
-  centerBoxStyle,
-  columnBoxStyle,
-  flexEndBoxStyle,
-  flexStartBoxStyle,
-  spaceBetweenBoxStyle,
-} from "~/core/style/boxStyles";
+import { centerBoxStyle, columnBoxStyle, flexEndBoxStyle, spaceBetweenBoxStyle } from "~/core/style/boxStyles";
 import { IButtonsWrapperProps } from "./types";
 import { blockProps } from "~/core/utils";
 
@@ -19,7 +13,6 @@ export const responseLayoutStyle: SxProps<Theme> = {
 export const StyledButtonsWrapper = styled(Box, {
   shouldForwardProp: blockProps("isFirstElement", "isLastElement"),
 })<IButtonsWrapperProps>(({ isFirstElement = false, isLastElement = false }) => {
-  if (isFirstElement === isLastElement) return { ...spaceBetweenBoxStyle };
-  if (isFirstElement) return { ...flexEndBoxStyle };
-  return { ...flexStartBoxStyle };
+  if (isLastElement || isFirstElement === isLastElement) return { ...spaceBetweenBoxStyle };
+  return { ...flexEndBoxStyle };
 });
