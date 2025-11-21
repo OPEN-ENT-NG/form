f576b23d75265133300f09d66d3c2ffb5a77456b
Quentin Périé <perieq@hotmail.fr>
2025-11-21 10:51:01 +0100

    feat(rgpd): [#FOR-991] add RGPD page in reponse flow (#544)

---
+++ formulaire/frontend/src/containers/RgpdLayout/style.ts
@@ -0,0 +1,20 @@
+import { SxProps, Theme } from "@cgi-learning-hub/ui";
+import { centerBoxStyle, columnBoxStyle } from "~/core/style/boxStyles";
+
+export const rgpdLayoutStyle: SxProps<Theme> = {
+  ...columnBoxStyle,
+  ...centerBoxStyle,
+  rowGap: "3rem",
+  paddingX: "10%",
+};
+
+export const rgpdStackStyle: SxProps<Theme> = {
+  width: "100%",
+  display: "flex",
+  flexDirection: "column",
+  borderRadius: 1,
+  marginBottom: 2,
+  padding: "3rem",
+  boxShadow: "1px 4px 5px 2px rgba(0, 0, 0, 0.1)",
+  rowGap: "2rem",
+};
