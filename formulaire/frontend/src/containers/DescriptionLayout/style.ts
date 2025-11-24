a295c9abaf20a7986dbc86fe8771cc36ec6f747f
Quentin Périé <perieq@hotmail.fr>
2025-11-24 17:57:38 +0100

    feat(preview): [#FOR-993] improve respond section style (#546)

--- formulaire/frontend/src/containers/DescriptionLayout/style.ts
+++ formulaire/frontend/src/containers/DescriptionLayout/style.ts
@@ -1,4 +1,5 @@
 import { SxProps, Theme } from "@cgi-learning-hub/ui";
+import { defaultPaperShadow } from "~/core/constants";
 import { centerBoxStyle, columnBoxStyle } from "~/core/style/boxStyles";
 
 export const descriptionLayoutStyle: SxProps<Theme> = {
@@ -9,12 +10,10 @@ export const descriptionLayoutStyle: SxProps<Theme> = {
 };
 
 export const descriptionStackStyle: SxProps<Theme> = {
-  width: "100%",
-  display: "flex",
-  flexDirection: "column",
+  ...columnBoxStyle,
   borderRadius: 1,
   marginBottom: 2,
   padding: "3rem",
-  boxShadow: "1px 4px 5px 2px rgba(0, 0, 0, 0.1)",
+  boxShadow: defaultPaperShadow,
   rowGap: "2rem",
 };
