d522a8acf380d89405bcd1f0c4542463e3118496
Quentin Périé <perieq@hotmail.fr>
2025-11-27 16:11:08 +0100

    feat(): [#FOR-913, #FOR-922] initialize preview response system (#548)
    * feat(): [#FOR-913, #FOR-922] initialize preview response system
    
    * feat(): [#FOR-913, #FOR-922] fix PR feedbacks

--- formulaire/frontend/src/providers/ResponseProvider/hook/useClassicResponse.tsx
+++ formulaire/frontend/src/providers/ResponseProvider/hook/useClassicResponse.tsx
@@ -1,12 +1,12 @@
 export const useClassicResponse = () => {
   // eslint-disable-next-line @typescript-eslint/require-await
-  const saveResponse = async () => {
+  const saveResponses = async () => {
     //TODO implement save logic here and remove linter disabler
     console.log("saving response for classic mode !");
     return;
   };
 
   return {
-    saveClassicResponse: saveResponse,
+    saveClassicResponses: saveResponses,
   };
 };
