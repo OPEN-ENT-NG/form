189f5d5d21335714d621afda5b068e0b3612342a
AMOSSE Nikolaï <niko@FR-GM7ZTL3.groupinfra.com>
2026-01-07 15:14:52 +0100

    wip

---
+++ formulaire/frontend/src/components/RespondQuestionTypes/RespondQuestionRanking/SortableQuestionItem/style.ts
@@ -0,0 +1,33 @@
+import { Box, Paper, styled } from "@cgi-learning-hub/ui";
+import { IGrapableBoxProps } from "../types";
+
+export const SortableQuestionPaper = styled(Paper)(({ theme }) => ({
+  display: "flex",
+  borderRadius: theme.shape.borderRadius,
+  justifyContent: "space-between",
+  marginBottom: "1.6rem",
+  alignItems: "center",
+  padding: "0.5rem 1rem",
+  backgroundColor: theme.palette.background.paper,
+  color: theme.palette.text.primary,
+}));
+
+export const GrapableBox = styled(Box, {
+  shouldForwardProp: (prop) => prop !== "isPreview",
+})<IGrapableBoxProps>(({ isPreview }) => {
+  return {
+    "&:hover": { cursor: isPreview ? "grabbing" : "grab" },
+  };
+});
+
+export const leftBoxStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: "1rem",
+};
+
+export const buttonsBoxStyle = {
+  display: "flex",
+};
+
+export const buttonStyle = { fontSize: "3rem" };
