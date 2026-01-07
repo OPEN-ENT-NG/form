269fba0fa93ca59f93817a781c4234dba4561202
niko-amosse <85460454+niko-amosse@users.noreply.github.com>
2026-01-07 15:11:26 +0100

    feat(preview): #FOR-865 save responses locally (#553)
    * feat(preview): #FOR-865 save responses locally
    
    * rm logs
    
    ---------
    
    Co-authored-by: AMOSSE Nikolaï <niko@FR-GM7ZTL3.groupinfra.com>

--- formulaire/frontend/src/components/RespondQuestionTypes/RespondQuestionSingleAnswerRadio/index.tsx
+++ formulaire/frontend/src/components/RespondQuestionTypes/RespondQuestionSingleAnswerRadio/index.tsx
@@ -1,16 +1,41 @@
-import { Box, FormControl, FormControlLabel, Radio, RadioGroup } from "@mui/material";
-import { FC } from "react";
+import { Box, FormControl, FormControlLabel, Radio, RadioGroup } from "@cgi-learning-hub/ui";
+import { ChangeEvent, FC, useEffect, useState } from "react";
+import { useResponse } from "~/providers/ResponseProvider";
 import { IRespondQuestionTypesProps } from "../types";
 
 export const RespondQuestionSingleAnswerRadio: FC<IRespondQuestionTypesProps> = ({ question }) => {
+  const { getQuestionResponse, updateQuestionResponses } = useResponse();
+  const [selectedValue, setSelectedValue] = useState<string>("");
+
+  useEffect(() => {
+    const associatedResponse = getQuestionResponse(question);
+    if (!associatedResponse) return;
+
+    const existingAnswer = associatedResponse.answer;
+    if (typeof existingAnswer === "string") {
+      setSelectedValue(existingAnswer);
+    }
+  }, [question, getQuestionResponse]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setSelectedValue(value);
+
+    const associatedResponse = getQuestionResponse(question);
+    if (!question.id || !associatedResponse) return;
+
+    associatedResponse.answer = value;
+    updateQuestionResponses(question, [associatedResponse]);
+  };
+
   return (
     <Box>
       <FormControl>
-        <RadioGroup>
+        <RadioGroup value={selectedValue || ""} onChange={handleChange}>
           {question.choices
             ?.sort((a, b) => a.position - b.position)
             .map((choice) => (
-              <FormControlLabel key={choice.id} value={choice.id ?? 0} control={<Radio />} label={choice.value} />
+              <FormControlLabel key={choice.id} value={choice.value} control={<Radio />} label={choice.value} />
             ))}
         </RadioGroup>
       </FormControl>
