e3f5aa2bb54b64b7e368ee86474e2bd649bf85f3
AMOSSE Nikolaï <niko@FR-GM7ZTL3.groupinfra.com>
2026-01-09 18:14:37 +0100

    feat(preview): #FOR-914 add matrix question

--- formulaire/frontend/src/providers/ResponseProvider/utils.ts
+++ formulaire/frontend/src/providers/ResponseProvider/utils.ts
@@ -1,7 +1,7 @@
 import { IFormElement } from "~/core/models/formElement/types";
 import { getStringifiedFormElementIdType, isQuestion, isSection } from "~/core/models/formElement/utils";
 import { QuestionTypes } from "~/core/models/question/enum";
-import { IQuestion } from "~/core/models/question/types";
+import { IQuestion, IQuestionChoice } from "~/core/models/question/types";
 import { IResponse } from "~/core/models/response/type";
 import { createNewResponse } from "~/core/models/response/utils";
 
@@ -9,25 +9,36 @@ export const initResponsesMap = (formElements: IFormElement[]) => {
   const responsesMap = new Map<string, Map<number, IResponse[]>>();
 
   formElements.forEach((formElement) => {
-    const formElementResponsesMap = new Map<number, IResponse[]>();
+    const formElementResponsesMap = new Map<IQuestion, IResponse[]>();
 
     if (isQuestion(formElement) && formElement.id) {
-      formElementResponsesMap.set(formElement.id, initResponseAccordingToType(formElement));
+      if (formElement.questionType === QuestionTypes.MATRIX) {
+        formElement.children?.forEach((child) => {
+          if (!child.id) return;
+          formElementResponsesMap.set(child, initResponseAccordingToType(child, formElement.choices));
+        });
+      } else {
+        formElementResponsesMap.set(formElement, initResponseAccordingToType(formElement));
+      }
     } else if (isSection(formElement)) {
       formElement.questions.forEach((question) => {
         if (!question.id) return;
-        formElementResponsesMap.set(question.id, initResponseAccordingToType(question));
+        formElementResponsesMap.set(question, initResponseAccordingToType(question));
       });
     }
 
-    const formElementIdType = getStringifiedFormElementIdType(formElement);
-    if (formElementIdType) responsesMap.set(formElementIdType, formElementResponsesMap);
+    formElementResponsesMap.forEach((responses, question) => {
+      const formElementIdType = getStringifiedFormElementIdType(question);
+      if (formElementIdType)
+        responsesMap.set(formElementIdType, new Map<number, IResponse[]>().set(question.id ?? 0, responses));
+    });
   });
   return responsesMap;
 };
 
-export const initResponseAccordingToType = (question: IQuestion): IResponse[] => {
+export const initResponseAccordingToType = (question: IQuestion, choices?: IQuestionChoice[] | null): IResponse[] => {
   if (!question.id) return [];
+  const questionChoices = choices ?? question.choices;
   switch (question.questionType) {
     case QuestionTypes.SHORTANSWER:
     case QuestionTypes.LONGANSWER:
@@ -39,15 +50,13 @@ export const initResponseAccordingToType = (question: IQuestion): IResponse[] =>
       return [createNewResponse(question.id)];
     case QuestionTypes.RANKING:
     case QuestionTypes.MULTIPLEANSWER:
-      return question.choices
-        ? question.choices.map((choice, index) =>
-            createNewResponse(question.id as number, undefined, choice.id as number, choice.value, index),
-          )
-        : [];
+      return (
+        questionChoices?.map((choice, index) =>
+          createNewResponse(question.id as number, undefined, choice.id as number, choice.value, index),
+        ) ?? []
+      );
     //TODO other question types
     case QuestionTypes.FILE:
-      return [];
-    case QuestionTypes.MATRIX:
       return [];
     default:
       return [];
