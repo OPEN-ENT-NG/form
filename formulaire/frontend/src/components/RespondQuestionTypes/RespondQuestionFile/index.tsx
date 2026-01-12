706819e279791de708d72d7137836c09cc395b42
AMOSSE Nikolaï <niko@FR-GM7ZTL3.groupinfra.com>
2026-01-12 17:34:18 +0100

    feedback

--- formulaire/frontend/src/components/RespondQuestionTypes/RespondQuestionFile/index.tsx
+++ formulaire/frontend/src/components/RespondQuestionTypes/RespondQuestionFile/index.tsx
@@ -1,5 +1,5 @@
 import { Box, Dropzone, FileList } from "@cgi-learning-hub/ui";
-import { FC, useEffect, useState } from "react";
+import { FC, useMemo } from "react";
 import { useTranslation } from "react-i18next";
 import { FORMULAIRE, MAX_FILES_SAVE } from "~/core/constants";
 import { IResponseFile } from "~/core/models/response/type";
@@ -9,15 +9,12 @@ import { ICustomFile } from "./types";
 import { createResponse, toCustomFile, toResponseFile } from "./utils";
 
 export const RespondQuestionFile: FC<IRespondQuestionTypesProps> = ({ question }) => {
-  const [files, setFiles] = useState<ICustomFile[]>([]);
   const { getQuestionResponse, updateQuestionResponses } = useResponse();
   const { t } = useTranslation(FORMULAIRE);
 
-  useEffect(() => {
+  const files = useMemo<ICustomFile[]>(() => {
     const associatedResponse = getQuestionResponse(question);
-
-    const newFiles: ICustomFile[] = associatedResponse?.files.map((file) => toCustomFile(file)) ?? [];
-    setFiles(newFiles);
+    return associatedResponse?.files.map((file) => toCustomFile(file)) ?? [];
   }, [question, getQuestionResponse]);
 
   const handleOnDrop = (acceptedFiles: File[]) => {
