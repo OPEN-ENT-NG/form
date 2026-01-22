0b547937cb95aeda6c7e8749de06c503f6cdb160
Florent Mariotti <florent.mariotti@cgi.com>
2026-01-22 17:24:38 +0100

    fix(preview):  [#FOR-1003] fix conditionnal answers (retour n°7 & 8)

--- formulaire/frontend/src/components/RespondQuestionTypes/RespondQuestionSingleAnswer/index.tsx
+++ formulaire/frontend/src/components/RespondQuestionTypes/RespondQuestionSingleAnswer/index.tsx
@@ -1,37 +1,64 @@
 import { Box } from "@cgi-learning-hub/ui";
-import { FormControl, MenuItem, Select, SelectChangeEvent } from "@mui/material";
-import { FC, useEffect, useState } from "react";
+import { FormControl, MenuItem, Select, SelectChangeEvent, TextField, Typography } from "@mui/material";
+import { ChangeEvent, FC, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 import { FORMULAIRE } from "~/core/constants";
 import { useResponse } from "~/providers/ResponseProvider";
 
 import { IRespondQuestionTypesProps } from "../types";
+import { IResponse } from "~/core/models/response/type";
 
 export const RespondQuestionSingleAnswer: FC<IRespondQuestionTypesProps> = ({ question }) => {
-  const { getQuestionResponse, updateQuestionResponses } = useResponse();
+  const { getQuestionResponses, updateQuestionResponses } = useResponse();
   const [selectedValue, setSelectedValue] = useState<string>("");
+  const [customAnswer, setCustomAnswer] = useState<string>("");
   const { t } = useTranslation(FORMULAIRE);
 
   useEffect(() => {
-    const associatedResponse = getQuestionResponse(question);
-    if (!associatedResponse) return;
+    const associatedResponses = getQuestionResponses(question);
 
-    const existingAnswer = associatedResponse.answer;
+    const selectedResponse = associatedResponses.find((response) => response.selected);
+
+    const existingAnswer = selectedResponse?.answer ?? "";
     if (typeof existingAnswer === "string") {
       setSelectedValue(existingAnswer);
     }
-  }, [question, getQuestionResponse]);
+    const customChoiceId = question.choices?.find((choice) => choice.isCustom)?.id;
+    if (!customChoiceId) return;
+    const customResponse = associatedResponses.find((response) => response.choiceId === customChoiceId);
+    if (customResponse) {
+      setCustomAnswer(customResponse.customAnswer ?? "");
+    }
+  }, [question, getQuestionResponses]);
 
   const handleChange = (event: SelectChangeEvent) => {
     const value = event.target.value;
     setSelectedValue(value);
 
-    const associatedResponse = getQuestionResponse(question);
-    if (!question.id || !associatedResponse) return;
+    const associatedResponses = getQuestionResponses(question);
+    const newResponses: IResponse[] = associatedResponses.map((response) => {
+      return { ...response, selected: response.answer === value };
+    });
 
-    associatedResponse.answer = value;
-    updateQuestionResponses(question, [associatedResponse]);
+    updateQuestionResponses(question, newResponses);
+  };
+
+  const handleCustomResponseChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newCustomAnswer = e.target.value;
+    const existingResponses = getQuestionResponses(question);
+
+    const customChoiceId = question.choices?.find((choice) => choice.isCustom)?.id;
+    if (!customChoiceId) return;
+
+    const updatedReponses = existingResponses.map((response) => {
+      if (response.choiceId === customChoiceId) {
+        return { ...response, customAnswer: newCustomAnswer };
+      }
+      return response;
+    });
+
+    updateQuestionResponses(question, updatedReponses);
   };
 
   return (
@@ -47,6 +74,18 @@ export const RespondQuestionSingleAnswer: FC<IRespondQuestionTypesProps> = ({ qu
               </MenuItem>
             ))}
         </Select>
+        {question.choices?.find((choice) => choice.value === selectedValue && choice.isCustom) && (
+          <>
+            <Typography sx={{ py: "1rem" }}>{t("formulaire.response.custom.label")} :</Typography>
+            <TextField
+              fullWidth
+              variant="standard"
+              value={customAnswer}
+              placeholder={t("formulaire.response.custom.write")}
+              onChange={handleCustomResponseChange}
+            />
+          </>
+        )}
       </FormControl>
     </Box>
   );
