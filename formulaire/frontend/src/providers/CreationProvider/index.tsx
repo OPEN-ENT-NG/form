3906c5d5a50b4d2dc6c36103170310510c41a953
Quentin Périé <perieq@hotmail.fr>
2026-01-22 12:44:29 +0100

    chore(): fix lint

--- formulaire/frontend/src/providers/CreationProvider/index.tsx
+++ formulaire/frontend/src/providers/CreationProvider/index.tsx
@@ -2,10 +2,6 @@ import { useEdificeClient } from "@edifice.io/react";
 import { createContext, FC, useCallback, useContext, useEffect, useMemo, useState } from "react";
 import { useParams } from "react-router-dom";
 import { IFolder } from "~/core/models/folder/types";
-import { useGetFormQuery } from "~/services/api/services/formulaireApi/formApi";
-import { useEdificeClient } from "@edifice.io/react";
-import { useRootFolders } from "../HomeProvider/utils";
-import { workflowRights } from "~/core/rights";
 import { IForm } from "~/core/models/form/types";
 import { IFormElement } from "~/core/models/formElement/types";
 import { isQuestion, isSection } from "~/core/models/formElement/utils";
@@ -17,15 +13,12 @@ import { useGetFoldersQuery } from "~/services/api/services/formulaireApi/folder
 import { useGetFormQuery } from "~/services/api/services/formulaireApi/formApi";
 import { useGetQuestionsQuery } from "~/services/api/services/formulaireApi/questionApi";
 import { useGetSectionsQuery } from "~/services/api/services/formulaireApi/sectionApi";
-import { initUserWorfklowRights, useRootFolders } from "../HomeProvider/utils";
+import { useGlobal } from "../GlobalProvider";
+import { useRootFolders } from "../HomeProvider/utils";
 import { useFormElementActions } from "./hook/useFormElementActions";
 import { useFormElementList } from "./hook/useFormElementsList";
 import { CreationProviderContextType, ICreationProviderProps } from "./types";
 import { isInFormElementsList, removeFormElementFromList, updateElementInList } from "./utils";
-import { IFolder } from "~/core/models/folder/types";
-import { isQuestion, isSection } from "~/core/models/formElement/utils";
-import { useGlobal } from "../GlobalProvider";
-import { getQuestionRootById, getQuestionSectionById } from "~/hook/dnd-hooks/useCreationDnd/utils";
 
 const CreationProviderContext = createContext<CreationProviderContextType | null>(null);
 
