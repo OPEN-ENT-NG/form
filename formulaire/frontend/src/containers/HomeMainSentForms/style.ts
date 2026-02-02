693527a896bc759377ecfb4e4fa5d8e9cbe40a32
AMOSSE Nikolaï <niko@FR-GM7ZTL3.groupinfra.com>
2026-02-02 10:17:49 +0100

    fix: #FOR-1004 fix form grid css (18)

--- formulaire/frontend/src/containers/HomeMainSentForms/style.ts
+++ formulaire/frontend/src/containers/HomeMainSentForms/style.ts
@@ -5,6 +5,7 @@ export const sentFormWrapperStyle: SxProps = {
   gridTemplateColumns: "repeat(auto-fill, minmax(min(30rem, 100%), 1fr))",
   gap: "1.6rem",
   width: "100%",
+  maxWidth: "100rem",
 };
 
 export const sentFormWrapperMobileStyle: SxProps = {
