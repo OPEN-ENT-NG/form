d5d9ee52e765f60f5da16321fd4bb9c581bf56a1
niko-amosse <85460454+niko-amosse@users.noreply.github.com>
2026-02-05 17:26:47 +0100

    feat(result): #FOR-936 add empty state for result page (#581)
    Co-authored-by: AMOSSE Nikolaï <niko@FR-GM7ZTL3.groupinfra.com>

--- formulaire/frontend/src/containers/result/ResultView/index.tsx
+++ formulaire/frontend/src/containers/result/ResultView/index.tsx
@@ -1,8 +1,10 @@
-import { Button, Paper, Select, Stack, Typography } from "@cgi-learning-hub/ui";
+import { Button, EmptyState, Link, Paper, Select, Stack, Typography } from "@cgi-learning-hub/ui";
 import { FC } from "react";
 
 import { Header } from "~/components/Header";
+import { EmptyResult } from "~/components/SVG/EmptyResult";
 import { ModalType } from "~/core/enums";
+import { FRONT_ROUTES } from "~/core/frontRoutes";
 import { ComponentVariant } from "~/core/style/themeProps";
 import { t } from "~/i18n";
 import { useGlobal } from "~/providers/GlobalProvider";
@@ -12,34 +14,50 @@ import { paperStyle } from "./style";
 import { getHeaderButtonsProps } from "./utils";
 
 export const ResultView: FC = () => {
-  const { form } = useResult();
+  const { form, countDistributions } = useResult();
   const { toggleModal } = useGlobal();
+
+  const buttons = countDistributions
+    ? getHeaderButtonsProps(
+        () => {
+          toggleModal(ModalType.FORM_RESULT_PDF);
+        },
+        () => {
+          toggleModal(ModalType.FORM_RESULT_CSV);
+        },
+      )
+    : [];
 
   return (
     <Stack width="100%">
-      <Header
-        items={[form.title, t("formulaire.results")]}
-        buttons={getHeaderButtonsProps(
-          () => {
-            toggleModal(ModalType.FORM_RESULT_PDF);
-          },
-          () => {
-            toggleModal(ModalType.FORM_RESULT_CSV);
-          },
-        )}
-        displaySeparator
-      />
-      <Stack margin={"1rem 10rem"}>
-        <Paper elevation={2} sx={paperStyle}></Paper>
-        <Stack direction="row" justifyContent="space-between" mt={3}>
-          <Button variant={ComponentVariant.CONTAINED}>{t("formulaire.prev")}</Button>
-          <Stack direction="row" alignItems="center" gap={2}>
-            <Typography>{t("formulaire.goTo")}</Typography>
-            <Select></Select>
+      <Header items={[form.title, t("formulaire.results")]} buttons={buttons} displaySeparator />
+      {countDistributions ? (
+        <Stack margin={"1rem 10rem"}>
+          <Paper elevation={2} sx={paperStyle}></Paper>
+          <Stack direction="row" justifyContent="space-between" mt={3}>
+            <Button variant={ComponentVariant.CONTAINED}>{t("formulaire.prev")}</Button>
+            <Stack direction="row" alignItems="center" gap={2}>
+              <Typography>{t("formulaire.goTo")}</Typography>
+              <Select></Select>
+            </Stack>
+            <Button variant={ComponentVariant.CONTAINED}>{t("formulaire.next")}</Button>
           </Stack>
-          <Button variant={ComponentVariant.CONTAINED}>{t("formulaire.next")}</Button>
         </Stack>
-      </Stack>
+      ) : (
+        <Stack width="100%" alignItems="center" gap={2}>
+          <EmptyState
+            mt={6}
+            image={<EmptyResult />}
+            title={t("formulaire.results.empty")}
+            description={t("formulaire.results.reminder")}
+            color="primary.main"
+            imageHeight={250}
+          />
+          <Link href={FRONT_ROUTES.home.path} color="primary">
+            {t("formulaire.error.backToMenu")}
+          </Link>
+        </Stack>
+      )}
     </Stack>
   );
 };
