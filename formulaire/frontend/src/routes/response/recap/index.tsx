78fb74b0e93ac59f6ba068cc24aa03e32fce97f6
Quentin Périé <perieq@hotmail.fr>
2026-02-11 17:42:09 +0100

    fix(): fix imports

--- formulaire/frontend/src/routes/response/recap/index.tsx
+++ formulaire/frontend/src/routes/response/recap/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 
-import { ResponseView } from "~/containers/ResponseView";
+import { ResponseView } from "~/containers/response/ResponseView";
 
 export const Recap: FC = () => {
   return <ResponseView />;
