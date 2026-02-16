81320ae628942edd9503b0e6a5ef66320c3e7ed9
niko-amosse <85460454+niko-amosse@users.noreply.github.com>
2026-02-16 12:02:12 +0100

    feat(result): #FOR-944 implements result view for response without graph (#586)
    * feat(result): #FOR-944 implement results map build
    
    * fix(result): #FOR-944 wip add short answer result
    
    * fix(result): #FOR-944 wip add short answer result
    
    * fix(result): #FOR-944 wip add short answer result
    
    * fix(result): #FOR-944 linter
    
    * fix(result): #FOR-944 wip files
    
    * fix(result): #FOR-944 end files
    
    * fix(result): #FOR-944 make feedback PR
    
    ---------
    
    Co-authored-by: AMOSSE Nikolaï <niko@FR-GM7ZTL3.groupinfra.com>

---
+++ formulaire/frontend/src/components/result/ResultRow/style.ts
@@ -0,0 +1,9 @@
+import { SxProps } from "@cgi-learning-hub/ui";
+
+import { CSS_DIVIDER_COLOR } from "~/core/style/cssColors";
+
+export const rowStyle: SxProps = {
+  border: `1px solid ${CSS_DIVIDER_COLOR}`,
+  borderRadius: "6px",
+  padding: "8px",
+};
