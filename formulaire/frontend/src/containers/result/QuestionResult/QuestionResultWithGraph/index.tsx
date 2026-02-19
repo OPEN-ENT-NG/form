b8c775b56ec242591ea76e0a9d962540c2702678
niko-amosse <85460454+niko-amosse@users.noreply.github.com>
2026-02-19 11:03:55 +0100

    feat(results): #FOR-937 add response stats left of graph (#593)
    * feat(result): #FOR-937 wip add response stats left to graph
    
    * fix(result): #FOR-937 end all results ok
    
    * fix(result): #FOR-937 make PR feedbacks
    
    ---------
    
    Co-authored-by: AMOSSE Nikolaï <niko@FR-GM7ZTL3.groupinfra.com>

---
+++ formulaire/frontend/src/containers/result/QuestionResult/QuestionResultWithGraph/index.tsx
@@ -0,0 +1,90 @@
+import { Box, Stack, Typography } from "@cgi-learning-hub/ui";
+import dayjs from "dayjs";
+import { FC, useMemo } from "react";
+
+import { ResultChart } from "~/components/result/ResultChart";
+import { ResultRow } from "~/components/result/ResultRow";
+import { compareChoices } from "~/containers/creation/CreationQuestionChoiceWrapper/utils";
+import { CHOICE_ID_FOR_NO_RESPONSE } from "~/core/constants";
+import { QuestionTypes } from "~/core/models/question/enum";
+import { ICompleteResponse } from "~/core/models/response/type";
+import { t } from "~/i18n";
+
+import { getDisplayDate } from "../utils";
+import { ChoiceId, IQuestionResultWithGraphProps, IResponseStats } from "./types";
+import { getDisplayedResponseStat, getResponseStatsMap } from "./utils";
+
+export const QuestionResultWithGraph: FC<IQuestionResultWithGraphProps> = ({ question, distributionMap }) => {
+  const allResponses: ICompleteResponse[] = Array.from(distributionMap.values()).flat();
+  const showLeftResponsesStat = useMemo(
+    () =>
+      question.questionType === QuestionTypes.SINGLEANSWER ||
+      question.questionType === QuestionTypes.SINGLEANSWERRADIO ||
+      question.questionType === QuestionTypes.MULTIPLEANSWER,
+    [question],
+  );
+  const sortedChoices = useMemo(() => [...(question.choices ?? [])].sort(compareChoices), [question]);
+  const responseStatsMap = useMemo(
+    () =>
+      showLeftResponsesStat ? getResponseStatsMap(sortedChoices, distributionMap) : new Map<ChoiceId, IResponseStats>(),
+    [sortedChoices, distributionMap, showLeftResponsesStat],
+  );
+  const customReponses = useMemo(() => {
+    const customChoice = question.choices?.find((choice) => choice.isCustom);
+    return allResponses
+      .filter((response) => response.choiceId === customChoice?.id && !!response.customAnswer)
+      .sort((a, b) => dayjs(b.dateResponse).diff(dayjs(a.dateResponse)));
+  }, [question, responseStatsMap]);
+
+  return (
+    <Stack gap={1}>
+      <Stack direction="row">
+        {showLeftResponsesStat && (
+          <Stack width="45%" gap={0.4}>
+            {sortedChoices.map((choice, index) => (
+              <Stack direction="row" justifyContent="space-between">
+                <Box>
+                  <Typography>{`${index + 1}. ${choice.value}`}</Typography>
+                </Box>
+                <Box width="15rem" minWidth="15rem">
+                  {getDisplayedResponseStat(choice.id, responseStatsMap, question)}
+                </Box>
+              </Stack>
+            ))}
+            {!question.mandatory && (
+              <Stack direction="row" justifyContent="space-between">
+                <Box>
+                  <Typography>{t("formulaire.response.empty")}</Typography>
+                </Box>
+                <Box width="15rem" minWidth="15rem">
+                  {getDisplayedResponseStat(CHOICE_ID_FOR_NO_RESPONSE, responseStatsMap, question)}
+                </Box>
+              </Stack>
+            )}
+          </Stack>
+        )}
+
+        <Box flex={1} minWidth={0}>
+          <ResultChart question={question} distributionMap={distributionMap} />
+        </Box>
+      </Stack>
+      {!!customReponses.length && (
+        <Stack gap={1}>
+          <Typography fontStyle="italic" color="text.secondary">
+            {t("formulaire.results.custom.answers")}
+          </Typography>
+          <Stack gap={1}>
+            {customReponses.map((customReponse) => (
+              <ResultRow
+                key={customReponse.id}
+                displayDate={getDisplayDate(customReponse)}
+                responderName={customReponse.responderName}
+                answer={customReponse.customAnswer}
+              />
+            ))}
+          </Stack>
+        </Stack>
+      )}
+    </Stack>
+  );
+};
---
+++ formulaire/frontend/src/containers/result/QuestionResult/QuestionResultWithGraph/utils.ts
@@ -0,0 +1,62 @@
+import { CHOICE_ID_FOR_NO_RESPONSE } from "~/core/constants";
+import { QuestionTypes } from "~/core/models/question/enum";
+import { IQuestion, IQuestionChoice } from "~/core/models/question/types";
+import { ICompleteResponse } from "~/core/models/response/type";
+import { t } from "~/i18n";
+import { DistributionMap } from "~/providers/ResultProvider/hook/UseBuildResultMap/types";
+
+import { ChoiceId, IResponseStats, ResponseStatsMap } from "./types";
+
+export const getResponseStatsMap = (choices: IQuestionChoice[], distributionMap: DistributionMap): ResponseStatsMap => {
+  const allResponses: ICompleteResponse[] = Array.from(distributionMap.values()).flat();
+
+  const total = distributionMap.size;
+
+  const countMap = allResponses.reduce<Map<ChoiceId, number>>((acc, response) => {
+    const choiceId = response.choiceId;
+
+    const isEmpty = !choiceId || (!response.answer && !response.customAnswer);
+
+    if (isEmpty) {
+      acc.set(CHOICE_ID_FOR_NO_RESPONSE, (acc.get(CHOICE_ID_FOR_NO_RESPONSE) ?? 0) + 1);
+      return acc;
+    }
+
+    acc.set(choiceId, (acc.get(choiceId) ?? 0) + 1);
+    return acc;
+  }, new Map());
+
+  const result: ResponseStatsMap = choices.reduce((acc, choice) => {
+    if (!choice.id) return acc;
+
+    const nbResponses = countMap.get(choice.id) ?? 0;
+
+    acc.set(choice.id, {
+      nbResponses,
+      percentage: (nbResponses / total) * 100,
+    });
+
+    return acc;
+  }, new Map<ChoiceId, IResponseStats>());
+
+  const emptyCount = countMap.get(CHOICE_ID_FOR_NO_RESPONSE) ?? 0;
+
+  result.set(CHOICE_ID_FOR_NO_RESPONSE, {
+    nbResponses: emptyCount,
+    percentage: (emptyCount / total) * 100,
+  });
+
+  return result;
+};
+
+export const getDisplayedResponseStat = (
+  choiceId: ChoiceId | null,
+  responseStatsMap: ResponseStatsMap,
+  question: IQuestion,
+) => {
+  const { nbResponses, percentage } = responseStatsMap.get(choiceId ?? -1) ?? { nbResponses: 0, percentage: 0 };
+
+  const i18nKey = question.questionType === QuestionTypes.MULTIPLEANSWER ? "formulaire.vote" : "formulaire.response";
+
+  return `${nbResponses} ${t(i18nKey, { count: nbResponses })} (${percentage.toFixed(2)}%)`;
+};
