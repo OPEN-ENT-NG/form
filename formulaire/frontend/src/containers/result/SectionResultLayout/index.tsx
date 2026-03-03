10e82530b9e2a1552e06403e8d268da88b43cf46
AMOSSE Nikolaï <niko@FR-GM7ZTL3.groupinfra.com>
2026-03-03 10:54:14 +0100

    fix(result): #FOR-1007 hide label no description for section (44)

--- formulaire/frontend/src/containers/result/SectionResultLayout/index.tsx
+++ formulaire/frontend/src/containers/result/SectionResultLayout/index.tsx
@@ -2,8 +2,7 @@ import { Stack, Typography } from "@cgi-learning-hub/ui";
 import { FC } from "react";
 
 import { getDescription, isDescriptionEmpty } from "~/components/CreationSection/utils";
-import { COMMON_WHITE_COLOR, TEXT_SECONDARY_COLOR } from "~/core/style/colors";
-import { t } from "~/i18n";
+import { COMMON_WHITE_COLOR } from "~/core/style/colors";
 
 import { sectionContentStyle, sectionTitleStyle } from "./style";
 import { SectionResultLayoutProps } from "./types";
@@ -15,12 +14,7 @@ export const SectionResultLayout: FC<SectionResultLayoutProps> = ({ section, chi
         {section.title}
       </Typography>
       <Stack sx={sectionContentStyle}>
-        {isDescriptionEmpty(section) ? (
-          <Typography color={TEXT_SECONDARY_COLOR} fontStyle="italic">
-            {" "}
-            {t("formulaire.section.no.description")}
-          </Typography>
-        ) : (
+        {!isDescriptionEmpty(section) && (
           <Typography dangerouslySetInnerHTML={{ __html: getDescription(section) || "" }} />
         )}
         {children}
