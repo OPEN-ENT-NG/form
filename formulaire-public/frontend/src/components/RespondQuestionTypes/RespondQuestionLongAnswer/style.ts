bc9067e57acd761691dc022264ed6eb70ce97a1b
Quentin Périé <perieq@hotmail.fr>
2026-03-05 18:15:57 +0100

    fix(editor): [#FOR-1007] hide buttons in public editor (n°78)

--- formulaire-public/frontend/src/components/RespondQuestionTypes/RespondQuestionLongAnswer/style.ts
+++ formulaire-public/frontend/src/components/RespondQuestionTypes/RespondQuestionLongAnswer/style.ts
@@ -2,4 +2,8 @@ export const respondQuestionLongAnswerStyle = {
   "& > :first-child": {
     minHeight: "200px",
   },
+  ".editor-toolbar .toolbar .toolbar-divider:not(.toolbar-divider ~ .toolbar-divider), button[aria-label='Ajout image'], button[aria-label='Ajout vidéo'], button[aria-label='Ajout audio'], button[aria-label='Ajout pièce jointe']":
+    {
+      display: "none !important",
+    },
 };
