d6e6a5779e6887a9b1e291b708af2d095f1fad8b
Florent Mariotti <florentcorse@gmail.com>
2026-03-05 16:49:09 +0100

    fix(public) : add enter press to validate send modal (retour 61) (#620)
    * wip
    
    * fix(public) : add enter press to validate send modal (retour 61)
    
    ---------
    
    Co-authored-by: Florent Mariotti <florent.mariotti@cgi.com>

--- formulaire-public/frontend/src/containers/SendingConfirmationModal/index.tsx
+++ formulaire-public/frontend/src/containers/SendingConfirmationModal/index.tsx
@@ -3,6 +3,7 @@ import { FC } from "react";
 
 import { TEXT_PRIMARY_COLOR } from "~/core/style/colors";
 import { BreakpointVariant, ComponentVariant, TypographyFontStyle, TypographyVariant } from "~/core/style/themeProps";
+import { isEnterPressed } from "~/core/utils";
 import { t } from "~/i18n";
 
 import { ISendingConfirmationModalProps } from "./types";
@@ -13,7 +14,14 @@ export const SendingConfirmationModal: FC<ISendingConfirmationModalProps> = ({
   onSendingConfirmation,
 }) => {
   return (
-    <Dialog open={isOpen} onClose={handleClose} maxWidth={BreakpointVariant.MD}>
+    <Dialog
+      open={isOpen}
+      onClose={handleClose}
+      maxWidth={BreakpointVariant.MD}
+      onKeyDown={(e) => {
+        if (isEnterPressed(e)) onSendingConfirmation();
+      }}
+    >
       <DialogTitle color={TEXT_PRIMARY_COLOR} variant={TypographyVariant.H2} fontWeight={TypographyFontStyle.BOLD}>
         {t("formulaire.public.responses.send.title")}
       </DialogTitle>
