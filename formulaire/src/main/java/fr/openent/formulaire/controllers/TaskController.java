7c55a9d47c002d034520ffa535b5cabb3ef9b0b1
mariusestaque <marius.estaque@opendigitaleducation.com>
2026-03-12 17:58:05 +0100

    chore: adding secured action

--- formulaire/src/main/java/fr/openent/formulaire/controllers/TaskController.java
+++ formulaire/src/main/java/fr/openent/formulaire/controllers/TaskController.java
@@ -3,6 +3,8 @@ package fr.openent.formulaire.controllers;
 import fr.openent.formulaire.cron.NotifyCron;
 import fr.openent.formulaire.cron.RgpdCron;
 import fr.wseduc.rs.Post;
+import fr.wseduc.security.ActionType;
+import fr.wseduc.security.SecuredAction;
 import fr.wseduc.webutils.http.BaseController;
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.impl.logging.Logger;
@@ -20,6 +22,7 @@ public class TaskController extends BaseController {
 	}
 
 	@Post("api/internal/delete-old-data-rgpd")
+	@SecuredAction(value = "", type = ActionType.RESOURCE)
 	public void deleteOldDataRgpd(HttpServerRequest request) {
 		log.info("Triggered old data RGPD task");
 		rgpdCron.handle(0L);
@@ -27,6 +30,7 @@ public class TaskController extends BaseController {
 	}
 
 	@Post("api/internal/launch-notifications")
+	@SecuredAction(value = "", type = ActionType.RESOURCE)
 	public void launchNotifications(HttpServerRequest request) {
 		log.info("Triggered notification task");
 		notifyCron.handle(0L);
