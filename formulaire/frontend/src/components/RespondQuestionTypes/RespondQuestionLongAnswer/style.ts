28987e5bfff1b99316c24d461eba5c68d285a2a2
Quentin Périé <perieq@hotmail.fr>
2026-03-23 16:55:03 +0100

    fix(): [#FOR-1012] fix focus paragraph response

--- formulaire/frontend/src/components/RespondQuestionTypes/RespondQuestionLongAnswer/style.ts
+++ formulaire/frontend/src/components/RespondQuestionTypes/RespondQuestionLongAnswer/style.ts
@@ -1,5 +1,16 @@
-export const respondQuestionLongAnswerStyle = {
+const flexContainerStyles = {
+  display: "flex",
+  width: "100%",
+
   "& > :first-child": {
+    flex: "1 1 auto",
+    width: "100%",
+  },
+};
+
+export const respondQuestionLongAnswerStyle = {
+  "& > :first-child > :nth-child(2)": {
     minHeight: "200px",
+    "> :first-child": flexContainerStyles,
   },
 };
