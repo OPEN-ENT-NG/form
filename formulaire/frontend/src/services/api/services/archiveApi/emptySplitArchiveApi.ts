6f66566322528fd52590438ac6d36fa9c154d958
Quentin Périé <perieq@hotmail.fr>
2026-04-07 16:54:27 +0200

    fix(import): [#FOR-1022] add xsfr token in request header

--- formulaire/frontend/src/services/api/services/archiveApi/emptySplitArchiveApi.ts
+++ formulaire/frontend/src/services/api/services/archiveApi/emptySplitArchiveApi.ts
@@ -5,7 +5,21 @@ import { TagName } from "~/core/enums";
 
 export const emptySplitArchiveApi = createApi({
   reducerPath: "archiveApi",
-  baseQuery: fetchBaseQuery({ baseUrl: `/${ARCHIVE}/` }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: `/${ARCHIVE}/`,
+    prepareHeaders: (headers) => {
+      const xsrfToken = document.cookie
+        .split("; ")
+        .find((row) => row.startsWith("XSRF-TOKEN="))
+        ?.split("=")[1];
+
+      if (xsrfToken) {
+        headers.set("X-XSRF-TOKEN", decodeURIComponent(xsrfToken));
+      }
+
+      return headers;
+    },
+  }),
   tagTypes: [TagName.FORMS],
   endpoints: () => ({}),
 });
